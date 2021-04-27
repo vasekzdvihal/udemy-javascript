442332a54fb4851c213d4028bf30a6da494a06d7
Václav Zdvihal <zdvihal@gmail.com>
2021-04-28 00:47:16 +0200

    Vue.js Monster Slayer - speacial attack

--- Vue/prj-monster-01-starting-setup/app.js
+++ Vue/prj-monster-01-starting-setup/app.js
@@ -1,5 +1,6 @@
 function getRandomValue(min, max) {
     const attackValue = Math.floor(Math.random() * (max - min)) + min;
+    return attackValue;
 }
 
 const app = {
@@ -7,17 +8,36 @@ const app = {
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0,
         };
+    },
+    computed: {
+      monsterBarStyles()  {
+         return {width: this.monsterHealth + '%'};
+      },
+      playerBarStyles() {
+          return {width: this.playerHealth + '%'};
+      },
+      mayUseSpecialAttack() {
+          return this.currentRound % 3 !== 0
+      }
     },
     methods: {
         attackMonster() {
+            this.currentRound++;
             const attackValue = getRandomValue(5, 12);
-            this.monsterHealth = this.monsterHealth - attackValue;
+            this.monsterHealth -= attackValue;
             this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandomValue(8 ,15);
-            this.playerHealth = this.playerHealth - attackValue;
+            this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(10 ,25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 };
