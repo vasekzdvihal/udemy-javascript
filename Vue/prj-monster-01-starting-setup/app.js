ba82fe4994baedf1e484331cca2462c2c9d8a989
Václav Zdvihal <zdvihal@gmail.com>
2021-04-28 00:52:19 +0200

    Vue.js Monster Slayer - heal

--- Vue/prj-monster-01-starting-setup/app.js
+++ Vue/prj-monster-01-starting-setup/app.js
@@ -38,6 +38,16 @@ const app = {
             const attackValue = getRandomValue(10 ,25);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if(this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
         }
     }
 };
