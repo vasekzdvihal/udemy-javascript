8e90f603bb12a1abb49327161249bc754bed82d0
Václav Zdvihal <zdvihal@gmail.com>
2021-04-28 23:55:37 +0200

    Vue.js Monster Slayer - core functionalites done

--- Vue/prj-monster-01-starting-setup/app.js
+++ Vue/prj-monster-01-starting-setup/app.js
@@ -14,9 +14,17 @@ const app = {
     },
     computed: {
       monsterBarStyles()  {
+          if(this.monsterHealth < 0) {
+              return { width: "0%" };
+          }
+
          return {width: this.monsterHealth + '%'};
       },
       playerBarStyles() {
+          if(this.playerHealth < 0) {
+              return { width: "0%" };
+          }
+
           return {width: this.playerHealth + '%'};
       },
       mayUseSpecialAttack() {
@@ -40,6 +48,12 @@ const app = {
       }
     },
     methods: {
+        startGame() {
+          this.monsterHealth = 100;
+          this.playerHealth = 100;
+          this.winner = null;
+          this.currentRound = 0;
+        },
         attackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
@@ -65,6 +79,9 @@ const app = {
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
+        },
+        surrender() {
+            this.winner = "monster";
         }
     }
 };
