73baeeb7ceeff8231c90098fc24d83999188204b
Václav Zdvihal <zdvihal@gmail.com>
2021-05-01 11:14:11 +0200

    Foldering projects

---
+++ Vue-TheCompleteGuide/04_BehindTheScenes/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,63 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue-TheCompleteGuide is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+     /* this.message = this.currentUserInput;*/
+      this.message = this.$refs.userText.value;
+    },
+  },
+});
+
+app.mount('#app');
+
+const app2 = Vue.createApp({
+  template: `
+    <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal : "Pizza!"
+    }
+  }
+});
+
+app2.mount("#app2");
+
+// ....
+
+/*let message = "Hello";
+let longMessage = message + " world";
+console.log(longMessage);
+
+message = "Hello??";
+console.log(longMessage);*/
+
+// .....
+
+/*
+const data = {
+  message: "Prd stary baby",
+  longMessage: ""
+};
+
+const handler = {
+  set(target, key, value) {
+    if(key === 'message') {
+      proxy.longMessage = value + " world!!!";
+    }
+    target.message = value;
+  }
+}
+
+const proxy = new Proxy(data, handler);
+proxy.message = "Hello!!!";
+console.log(proxy.longMessage);
+console.log(proxy.message);*/
