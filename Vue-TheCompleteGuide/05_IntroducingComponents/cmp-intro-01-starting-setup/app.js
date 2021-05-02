0525968b4459946721dd5100e55c834d548d3487
Václav Zdvihal <zdvihal@gmail.com>
2021-05-02 19:22:50 +0200

    Vue.js Introducing Components, intro

--- Vue-TheCompleteGuide/05_IntroducingComponents/cmp-intro-01-starting-setup/app.js
+++ Vue-TheCompleteGuide/05_IntroducingComponents/cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
    data() {
        return {
-           detailsAreVisible: false,
            friends : [
                {
                    id : 'manuel',
@@ -17,11 +16,35 @@ const app = Vue.createApp({
                },
            ]
        }
-   },
+   }
+});
+
+app.component('friend-contact', {
+    template: `
+      <li :key="friend.id">
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+      </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible : false,
+            friend: {
+                id : 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '01234 5678 991',
+                email: 'manuel@localhost.com'
+            },
+        }
+    },
     methods: {
-       toggleDetails() {
-           this.detailsAreVisible = !this.detailsAreVisible;
-       }
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
     }
 });
 
