05383df8876809cf0effbd91535e86984ccdd367
Václav Zdvihal <zdvihal@gmail.com>
2021-05-02 20:55:48 +0200

    Vue.js - Vue CLI, App.vue

---
+++ Vue-TheCompleteGuide/06_MovingToVueCLI/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0123 45678 991',
+          email: 'manual@localhost.com'
+        },
+        {
+          id: 'julia',
+          name: 'Julia Jones',
+          phone: '0123 454334 2321',
+          email: 'julia@localhost.com'
+        }
+      ]
+    };
+  }
+};
+</script>
