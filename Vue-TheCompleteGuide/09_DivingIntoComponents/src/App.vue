8ea07a0a79369d41856a6b1494e8025763dcde88
Václav Zdvihal <zdvihal@gmail.com>
2021-05-26 23:57:27 +0200

    Vue.js - Diving Into Components, Scoped Slots

--- Vue-TheCompleteGuide/09_DivingIntoComponents/src/App.vue
+++ Vue-TheCompleteGuide/09_DivingIntoComponents/src/App.vue
@@ -7,26 +7,34 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+      <!-- Kdyz je jen jeden slot nemusi byt template -->
+      <!-- <template #default="slotProps"> -->
+      <h2>{{ slotProps.item }}</h2>
+      <!-- </template> -->
+    </course-goals>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   components: {
-    'the-header': TheHeader,
-    'badge-list': BadgeList,
-    'user-info':UserInfo
+    "the-header": TheHeader,
+    "badge-list": BadgeList,
+    "user-info": UserInfo,
+    "course-goals": CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
