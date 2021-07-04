d9332171eb0a6aa8fcd1f6ff5746b89958053340
Václav Zdvihal <zdvihal@gmail.com>
2021-07-04 23:51:08 +0200

    Vue.js - The Learning Resource App, Adding a Modal Dialog

--- Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/components/learning-resources/AddResource.vue
+++ Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least on input is invalid.</p>
+      <p>Please check your inputs.</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -23,13 +32,26 @@
 <script>
 export default {
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    }
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDescription = this.$refs.descInput.value;
       const linkTitle = this.$refs.linkInput.value;
 
+      if(enteredTitle.trim() === '' || enteredDescription.trim() === '' || linkTitle.trim() === '') {
+        this.inputIsInvalid = true;
+        return;
+      }
+
       this.addResource(enteredTitle, enteredDescription, linkTitle);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     }
   }
 }
