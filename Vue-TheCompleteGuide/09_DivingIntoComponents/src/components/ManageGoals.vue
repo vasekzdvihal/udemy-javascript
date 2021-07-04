2445736c7d90faecf527d7cb8831d1fb4d3fa974
Václav Zdvihal <zdvihal@gmail.com>
2021-07-04 11:05:39 +0200

    Vue.js - Diving Into Components, Applying what we know & a problem, error dialog

--- Vue-TheCompleteGuide/09_DivingIntoComponents/src/components/ManageGoals.vue
+++ Vue-TheCompleteGuide/09_DivingIntoComponents/src/components/ManageGoals.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please input something.</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+  import ErrorAlert from "./ErrorAlert";
+  export default {
+    components: {ErrorAlert},
+    data() {
+      return {
+        inputIsInvalid: false
+      }
+    },
+    methods: {
+      setGoal() {
+        const enteredValue = this.$refs.goal.value;
+        if(enteredValue === '') {
+          this.inputIsInvalid = true;
+        }
+      },
+      confirmError() {
+        this.inputIsInvalid = false;
+      }
+    }
+  }
+</script>
