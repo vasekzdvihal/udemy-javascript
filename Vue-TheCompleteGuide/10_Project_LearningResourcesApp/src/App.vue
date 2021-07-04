859c0c09f50bc44b99a394d33cc2e9036f02e145
Václav Zdvihal <zdvihal@gmail.com>
2021-07-04 15:12:21 +0200

    Vue.js - The Learning Resource App, Setup and First Steps

---
+++ Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul v-for="storedResources"></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources : [
+        { id: 'official-guide', title: 'Official Guide', description: 'This is short description', link: 'https://vuejs.org' }
+        { id: 'google', title: 'Google', description: 'Google', link: 'https://google.com' }
+      ]
+    }
+  }
+}
+</script>
