0e8ac29c3c374671d5b050461afae194a66b2ede
Václav Zdvihal <zdvihal@gmail.com>
2021-07-04 23:37:28 +0200

    Vue.js - The Learning Resource App, Fetching user input

--- Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/components/learning-resources/TheResources.vue
+++ Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/components/learning-resources/TheResources.vue
@@ -3,7 +3,9 @@
     <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
     <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -33,12 +35,24 @@ export default {
   },
   provide() {
     return {
-      resources: this.storedResources
+      resources: this.storedResources,
+      addResource: this.addResource
     }
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url
+      };
+
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
     }
   }
 }
