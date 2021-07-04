6518cd06eeb3fbecc2011a83602fef8583a1db22
Václav Zdvihal <zdvihal@gmail.com>
2021-07-04 16:44:20 +0200

    Vue.js - The Learning Resource App, First Components and Props

--- Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/App.vue
+++ Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <ul v-for="storedResources"></ul>
+  <ul>
+    <learning-resource v-for="res in storedResources" :key="res.id" :title="res.title" :description="res.description" :link="res.link"></learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource';
+
 export default {
+  components: {
+    LearningResource
+  },
   data() {
     return {
       storedResources : [
-        { id: 'official-guide', title: 'Official Guide', description: 'This is short description', link: 'https://vuejs.org' }
+        { id: 'official-guide', title: 'Official Guide', description: 'This is short description', link: 'https://vuejs.org' },
         { id: 'google', title: 'Google', description: 'Google', link: 'https://google.com' }
       ]
     }
