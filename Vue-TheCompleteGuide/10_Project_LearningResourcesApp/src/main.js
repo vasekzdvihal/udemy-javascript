f6914dd4170dc01299a80a06f58139c4247a37cb
Václav Zdvihal <zdvihal@gmail.com>
2021-07-04 17:01:06 +0200

    Vue.js - The Learning Resource App, Header and BaseCard component

--- Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/main.js
+++ Vue-TheCompleteGuide/10_Project_LearningResourcesApp/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import BaseCard from './components/UI/BaseCard'
 
-createApp(App).mount('#app');
+
+const app = createApp(App);
+
+app.component('base-card', BaseCard);
+
+app.mount('#app');
