f0aac91e09e9c9c58f3d400b5e2fdba261b7056a
Václav Zdvihal <zdvihal@gmail.com>
2021-07-14 23:13:10 +0200

    Vue.js - Routing, Redirecting and Catch All Routes

---
+++ Vue-TheCompleteGuide/13_Routing/src/components/nav/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Page not found! Mybe view our <router-link to="/teams">teams</router-link></h2>
+</template>
--- Vue-TheCompleteGuide/13_Routing/src/main.js
+++ Vue-TheCompleteGuide/13_Routing/src/main.js
@@ -5,13 +5,17 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList';
 import TeamMembers from "./components/teams/TeamMembers";
 import UsersList from './components/users/UsersList';
+import NotFound from './components/nav/NotFound';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        { path: '/', redirect: '/teams' },
         { path: '/teams', component: TeamsList },
+        //{ path: '/teams', component: TeamsList, alias: '/' },
         { path: '/teams/:teamId', component: TeamMembers, props: true },
         { path: '/users', component: UsersList },
+        { path: '/:catchAll(.*)', component: NotFound }
     ],
     linkActiveClass: 'active'
 });
