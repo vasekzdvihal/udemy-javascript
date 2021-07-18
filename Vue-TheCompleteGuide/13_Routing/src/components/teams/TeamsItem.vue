ec1b3f5836e85cbd13f004c90b765a79388f5122
Václav Zdvihal <zdvihal@gmail.com>
2021-07-18 23:20:05 +0200

    Vue.js - Routing, Using Query Params

--- Vue-TheCompleteGuide/13_Routing/src/components/teams/TeamsItem.vue
+++ Vue-TheCompleteGuide/13_Routing/src/components/teams/TeamsItem.vue
@@ -12,7 +12,13 @@ export default {
   props: ['id', 'name', 'memberCount'],
   computed: {
     teamMembersLink() {
-      return { name: 'team-members', params: { teamId: this.id } }
+      // return '/teams/' + this.id + '?sort=asc';
+      return {
+        name: 'team-members',
+        params: { teamId: this.id },
+        query: { sort: 'asc' },
+      };
+      // this.$router.push({ name: 'team-members', params: { teamId: this.id } });
     }
   }
 };
--- Vue-TheCompleteGuide/13_Routing/src/main.js
+++ Vue-TheCompleteGuide/13_Routing/src/main.js
@@ -2,25 +2,32 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList';
-import TeamMembers from "./components/teams/TeamMembers";
-import UsersList from './components/users/UsersList';
-import NotFound from './components/nav/NotFound';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/teams' },
-        { name: 'teams', path: '/teams', component: TeamsList, children: [
-            { name: 'team-member', path: ':teamId', component: TeamMembers, props: true },
-            ]},
-        //{ path: '/teams', component: TeamsList, alias: '/' },
+        {
+            name: 'teams',
+            path: '/teams',
+            component: TeamsList,
+            children: [
+                { name: 'team-members', path: ':teamId', component: TeamMembers, props: true } // /teams/t1
+            ]
+        }, // our-domain.com/teams => TeamsList
         { path: '/users', component: UsersList },
-        { path: '/:catchAll(.*)', component: NotFound },
+
+        { path: '/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass: 'active'
 });
 
-const app = createApp(App)
+const app = createApp(App);
+
 app.use(router);
+
 app.mount('#app');
