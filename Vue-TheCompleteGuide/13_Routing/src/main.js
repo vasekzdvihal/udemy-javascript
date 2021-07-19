7a6ff4a492a9a40ec82c6ccf9231841c8cfb58b3
Václav Zdvihal <zdvihal@gmail.com>
2021-07-19 23:03:33 +0200

    Vue.js - Routing, Controlling Scroll Behaviour

--- Vue-TheCompleteGuide/13_Routing/src/main.js
+++ Vue-TheCompleteGuide/13_Routing/src/main.js
@@ -26,7 +26,14 @@ const router = createRouter({
             }},
         { path: '/:notFound(.*)', component: NotFound }
     ],
-    linkActiveClass: 'active'
+    linkActiveClass: 'active',
+    scrollBehavior(to, from, savedPosition) {
+        console.log(to, from, savedPosition);
+        if(savedPosition) {
+            return savedPosition;
+        }
+        return { left: 0, top: 0};
+    }
 });
 
 const app = createApp(App);
