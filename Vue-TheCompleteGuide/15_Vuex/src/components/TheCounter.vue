c351bdc374969890b9b2e20fc62dc8b5404f570a
Václav Zdvihal <zdvihal@gmail.com>
2021-08-14 10:00:50 +0200

    Vue.js - Vuex, Connecting Components to State

---
+++ Vue-TheCompleteGuide/15_Vuex/src/components/TheCounter.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>{{ counted }}</h3>
+</template>
+
+<script>
+export default {
+  name: "TheCounter",
+  computed: {
+    counted() {
+      return this.$store.state.counter;
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
