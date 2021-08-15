ab98cd99a589bbd4baf8dce26401ace6af9f8256
Václav Zdvihal <zdvihal@gmail.com>
2021-08-15 20:39:25 +0200

    Vue.js - Vuex, Challange I

---
+++ Vue-TheCompleteGuide/15_Vuex_Challenge/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <the-header></the-header>
+  <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  provide() {
+    return {
+      isLoggedIn: this.isLoggedIn,
+      cart: this.cart,
+      addProductToCart: this.addProductToCart,
+      removeProductFromCart: this.removeProductFromCart,
+      login: this.login,
+      logout: this.logout,
+    };
+  },
+  methods: {
+    login() {
+      this.isLoggedIn = true;
+    },
+    logout() {
+      this.isLoggedIn = false;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
