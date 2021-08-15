6745b85fbfcedbf018989d58d395e051b30142ae
Václav Zdvihal <zdvihal@gmail.com>
2021-08-15 17:03:51 +0200

    Vue.js - Vuex, Organizing your Store with Modules

--- Vue-TheCompleteGuide/15_Vuex/src/main.js
+++ Vue-TheCompleteGuide/15_Vuex/src/main.js
@@ -3,34 +3,62 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+const counterModule = {
+    state() {
+        return {
+            counter: 0,
+        };
+    },
+    mutations: {
+        increment(state) {
+            state.counter++;
+        },
+        increase(state, payload) {
+            state.counter = state.counter + payload.value;
+        },
+    },
+    actions: {
+        increment(context) {
+            setTimeout(function() {
+                context.commit('increment');
+            }, 2000);
+        },
+        increase(context, payload) {
+            console.log(context);
+            context.commit('increase', payload)
+        },
+    },
+    getters: {
+        finalCounter(state) {
+            return state.counter * 3;
+        },
+        normalizeCounter(_, getters) { // pouziva se kdyz to prvni nepouzivam ale potrebuji to abych se dostal ke druhymu
+            if(getters.finalCounter < 0) {
+                return 0;
+            }
+            if(getters.finalCounter > 100) {
+                return 100;
+            }
+            return getters.finalCounter;
+        },
+    }
+}
+
 const store = createStore({
+   modules: {
+     numbers: counterModule,
+   },
    state() {
        return {
-           counter: 0,
            isLoggedIn: false,
        };
    },
     mutations: {
-       increment(state) {
-           state.counter++;
-       },
-       increase(state, payload) {
-           state.counter = state.counter + payload.value;
-       },
        setAuth(state, payload) {
            state.isLoggedIn = payload.isAuth;
        }
     },
     actions: {
-      increment(context) {
-          setTimeout(function() {
-              context.commit('increment');
-          }, 2000);
-      },
-      increase(context, payload) {
-          console.log(context);
-          context.commit('increase', payload)
-      },
       login(context) {
           context.commit('setAuth', { isAuth: true });
       },
@@ -39,18 +67,6 @@ const store = createStore({
       },
     },
     getters: {
-       finalCounter(state) {
-           return state.counter * 3;
-       },
-       normalizeCounter(_, getters) { // pouziva se kdyz to prvni nepouzivam ale potrebuji to abych se dostal ke druhymu
-           if(getters.finalCounter < 0) {
-               return 0;
-           }
-           if(getters.finalCounter > 100) {
-               return 100;
-           }
-           return getters.finalCounter;
-       },
        userIsAuthenticated(state) {
            return state.isLoggedIn;
        }
