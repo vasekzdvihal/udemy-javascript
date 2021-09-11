2b623f49831d503b803d44c16bab124c458acb65
Václav Zdvihal <zdvihal@gmail.com>
2021-09-11 21:45:58 +0200

    Vue.js - Main Project, Outputting Incoming Requests for the Active Coach

--- Vue-TheCompleteGuide/16_MainProject/src/pages/requests/RequestsReceived.vue
+++ Vue-TheCompleteGuide/16_MainProject/src/pages/requests/RequestsReceived.vue
@@ -1,13 +1,47 @@
 <template>
-  <h3>requests</h3>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <requests-item v-for="req in receivedRequests" :key="req.id" :email="req.userEmail" :message="req.message" />
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import RequestsItem from "../../components/requests/RequestsItem";
+
 export default {
-  name: "RequestsReceived"
+  name: "RequestsReceived",
+  components: { RequestsItem },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    }
+  }
 }
 </script>
 
 <style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
