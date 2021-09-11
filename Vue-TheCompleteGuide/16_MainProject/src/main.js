7fa0822521dbc229fb854341bea9c32d136ac01f
Václav Zdvihal <zdvihal@gmail.com>
2021-09-11 23:05:10 +0200

    Vue.js - Main Project, Adding Http Error Handling

--- Vue-TheCompleteGuide/16_MainProject/src/main.js
+++ Vue-TheCompleteGuide/16_MainProject/src/main.js
@@ -7,6 +7,7 @@ import BaseCard from "./components/ui/BaseCard";
 import BaseBadge from "./components/ui/BaseBadge";
 import BaseButton from "./components/ui/BaseButton"
 import BaseSpinner from "./components/ui/BaseSpinner";
+import BaseDialog from "./components/ui/BaseDialog";
 
 const app = createApp(App)
 
@@ -17,5 +18,6 @@ app.component('base-card', BaseCard);
 app.component('base-badge', BaseBadge);
 app.component('base-button', BaseButton);
 app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
