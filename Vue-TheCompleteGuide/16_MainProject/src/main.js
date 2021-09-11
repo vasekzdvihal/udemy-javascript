ab41d220c9b0751679d9affcef88ec41035097f2
Václav Zdvihal <zdvihal@gmail.com>
2021-09-11 22:45:17 +0200

    Vue.js - Main Project, Rendering a Loading Spinner

--- Vue-TheCompleteGuide/16_MainProject/src/main.js
+++ Vue-TheCompleteGuide/16_MainProject/src/main.js
@@ -5,7 +5,8 @@ import App from './App';
 
 import BaseCard from "./components/ui/BaseCard";
 import BaseBadge from "./components/ui/BaseBadge";
-import BaseButton from "./components/ui/BaseButton";
+import BaseButton from "./components/ui/BaseButton"
+import BaseSpinner from "./components/ui/BaseSpinner";
 
 const app = createApp(App)
 
@@ -15,5 +16,6 @@ app.use(store);
 app.component('base-card', BaseCard);
 app.component('base-badge', BaseBadge);
 app.component('base-button', BaseButton);
+app.component('base-spinner', BaseSpinner);
 
 app.mount('#app');
--- Vue-TheCompleteGuide/16_MainProject/src/pages/coaches/CoachesList.vue
+++ Vue-TheCompleteGuide/16_MainProject/src/pages/coaches/CoachesList.vue
@@ -6,9 +6,12 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
+        <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
             v-for="coach in filteredCoaches"
             :id="coach.id"
@@ -34,6 +37,7 @@ export default {
   components: { CoachItem, CoachFilter },
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -52,7 +56,7 @@ export default {
        });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
     isCoach() {
       return this.$store.getters['coaches/isCoach'];
@@ -65,8 +69,10 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches');
+    async loadCoaches() {
+      this.isLoading = true;
+      await this.$store.dispatch('coaches/loadCoaches');
+      this.isLoading = false;
     },
   }
 }
