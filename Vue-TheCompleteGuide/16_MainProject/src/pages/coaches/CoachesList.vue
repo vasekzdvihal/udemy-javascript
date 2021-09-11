af98e7c1ced9aa52510ab54bbe4032f048d7fec7
Václav Zdvihal <zdvihal@gmail.com>
2021-09-11 22:34:42 +0200

    Vue.js - Main Project, Fetching Coach Data (GET Http Request)

--- Vue-TheCompleteGuide/16_MainProject/src/pages/coaches/CoachesList.vue
+++ Vue-TheCompleteGuide/16_MainProject/src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
       </div>
       <ul v-if="hasCoaches">
@@ -58,9 +58,15 @@ export default {
       return this.$store.getters['coaches/isCoach'];
     }
   },
+  created() {
+    this.loadCoaches();
+  },
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
     },
   }
 }
--- Vue-TheCompleteGuide/16_MainProject/src/store/modules/coaches/actions.js
+++ Vue-TheCompleteGuide/16_MainProject/src/store/modules/coaches/actions.js
@@ -14,7 +14,6 @@ export default {
             body: JSON.stringify(coachData),
         });
 
-        // const responseData = await response.json();
         if(!response.ok) {
             // error ...
         }
@@ -23,4 +22,26 @@ export default {
             id: userId
         });
     },
+    async loadCoaches(context) {
+        const response = await fetch(`https://vue-http-demo2-fc296-default-rtdb.firebaseio.com/coaches.json`);
+        const responseData = await response.json();
+        if(!response.ok) {
+            // error...
+        }
+
+        const coaches = [];
+
+        for( const key in responseData) {
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas
+            };
+            coaches.push(coach);
+        }
+        context.commit('setCoaches', coaches);
+    }
 };
