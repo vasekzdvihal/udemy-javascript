6ac8cba6f89a4919d698e833bb9cb502e4144869
Václav Zdvihal <zdvihal@gmail.com>
2021-09-11 13:29:10 +0200

    Vue.js - Main Project, Filtering Coaches

--- Vue-TheCompleteGuide/16_MainProject/src/pages/coaches/CoachesList.vue
+++ Vue-TheCompleteGuide/16_MainProject/src/pages/coaches/CoachesList.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    FILTER
+    <coach-filter @change-filter="setFilters"></coach-filter>
   </section>
   <section>
     <base-card>
@@ -27,18 +27,38 @@
 
 <script>
 import CoachItem from "../../components/coaches/CoachItem";
-import BaseButton from "../../components/ui/BaseButton";
+import CoachFilter from "../../components/coaches/CoachFilter";
 
 export default {
   name: "CoachesList",
-  components: {BaseButton, CoachItem },
+  components: { CoachItem, CoachFilter },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      }
+    };
+  },
   computed: {
     filteredCoaches() {
-      return this.$store.getters['coaches/coaches'];
+       const coaches = this.$store.getters['coaches/coaches'];
+       return coaches.filter(coach => {
+         if (this.activeFilters.frontend && coach.areas.includes('frontend')) { return true; }
+         if (this.activeFilters.backend && coach.areas.includes('backend')) { return true; }
+         if (this.activeFilters.career && coach.areas.includes('career')) { return true; }
+         return false;
+       });
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
     }
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
+    },
   }
 }
 </script>
