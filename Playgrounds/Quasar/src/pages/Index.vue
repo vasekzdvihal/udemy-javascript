ce9b5fff76d180bcf93b31fe0174480f8d7574cc
Vášek Zdvihal <zdvihal@gmail.com>
2021-09-13 09:57:48 +0200

    Showcase Quasar for JJ

--- Playgrounds/Quasar/src/pages/Index.vue
+++ Playgrounds/Quasar/src/pages/Index.vue
@@ -1,10 +1,35 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <section>
+      <q-card class="q-pa-lg q-ma-lg flex column flex-center">
+        <span class="text-h6">Colors</span>
+        <q-btn class="q-ma-xs full-width" color="primary">Primary</q-btn>
+        <q-btn class="q-ma-xs full-width" color="secondary">Secondary</q-btn>
+        <q-btn class="q-ma-xs full-width" color="accent">Accent</q-btn>
+        <q-btn class="q-ma-xs full-width" color="dark">Dark</q-btn>
+        <q-btn class="q-ma-xs full-width" color="positive">Positive</q-btn>
+        <q-btn class="q-ma-xs full-width" color="negative">Negative</q-btn>
+        <q-btn class="q-ma-xs full-width" color="info">Info</q-btn>
+        <q-btn class="q-ma-xs full-width" color="warning">Warning</q-btn>
+      </q-card>
+    </section>
+    <section>
+      <q-card class="q-pa-lg q-ma-lg flex column flex-center">
+        <span class="text-h6">Backgrounds</span>
+        <div class="q-ma-xs q-pa-lg bg-primary">
+          <span>This is Primary background</span>
+        </div>
+        <div class="q-ma-xs q-pa-lg bg-secondary">
+          <span>This is Primary background</span>
+        </div>
+        <div class="q-ma-xs q-pa-lg bg-accent">
+          <span>This is Primary background</span>
+        </div>
+        <div class="q-ma-xs q-pa-lg bg-dark text-white">
+          <span>This is Dark background with white text</span>
+        </div>
+      </q-card>
+    </section>
   </q-page>
 </template>
 
