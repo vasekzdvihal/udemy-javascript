902c73f23eb4bbedd490dea2a1d5a9fe3834ae40
Vášek Zdvihal <zdvihal@gmail.com>
2021-10-17 11:51:51 +0200

    Vue.js - Authentication, Attaching the Token to Outgoing Requests

--- Udemy/Vue-TheCompleteGuide/17_Authentication/src/store/modules/auth/actions.js
+++ Udemy/Vue-TheCompleteGuide/17_Authentication/src/store/modules/auth/actions.js
@@ -1,5 +1,29 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCuR2PNHoygNfrJNLb0rhNyJfSsURklK6Y', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      })
+    });
+
+    const responseData = await response.json();
+
+    if(!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || 'Failed to authenticate.');
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCuR2PNHoygNfrJNLb0rhNyJfSsURklK6Y', {
       method: 'POST',
