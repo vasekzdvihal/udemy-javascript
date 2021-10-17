430b4bfa5666c1deb2af52ee698edc5cd3df794a
Vášek Zdvihal <zdvihal@gmail.com>
2021-10-17 10:15:39 +0200

    Vue.js - Authentication, Redirect to my Firebase

--- Udemy/Vue-TheCompleteGuide/17_Authentication/src/store/modules/requests/actions.js
+++ Udemy/Vue-TheCompleteGuide/17_Authentication/src/store/modules/requests/actions.js
@@ -4,7 +4,7 @@ export default {
       userEmail: payload.email,
       message: payload.message
     };
-    const response = await fetch(`https://vue-http-demo2-fc296-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
+    const response = await fetch(`https://vue-http-demo3-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
       method: 'POST',
       body: JSON.stringify(newRequest)
     });
@@ -23,7 +23,7 @@ export default {
   },
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
-    const response = await fetch(`https://vue-http-demo2-fc296-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+    const response = await fetch(`https://vue-http-demo3-default-rtdb.firebaseio.com/requests/${coachId}.json`);
     const responseData = await response.json();
 
     if (!response.ok) {
