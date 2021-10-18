c6fd03ae82116c3a1273c8a897cce32696bd3a53
Vášek Zdvihal <zdvihal@gmail.com>
2021-10-18 21:57:41 +0200

    Vue.js - Deploying, NOTE:Not Recomended async components for routes

--- Udemy/Vue-TheCompleteGuide/18_Deploying/src/router.js
+++ Udemy/Vue-TheCompleteGuide/18_Deploying/src/router.js
@@ -10,11 +10,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 import NotFound from './pages/NotFound.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 
-const CoachDetail = defineAsyncComponent(() => import('./pages/coaches/CoachDetail.vue'));
-const CoachRegistration = defineAsyncComponent(() => import('./pages/coaches/CoachRegistration.vue'));
-const ContactCoach = defineAsyncComponent(() => import('./pages/requests/ContactCoach.vue'));
-const RequestsReceived = defineAsyncComponent(() => import('./pages/requests/RequestsReceived.vue'));
-const UserAuth = defineAsyncComponent(() => import('./pages/auth/UserAuth.vue'));
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 import store from './store';
 
