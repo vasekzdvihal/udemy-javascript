c95cb8c75368296c3f972f1efb4b017683e191ee
Vášek Zdvihal <zdvihal@gmail.com>
2021-10-31 17:17:37 +0100

    Vue.js - Composition API, Building reactive objects

--- Udemy/Vue-TheCompleteGuide/19_CompositionAPI/src/App.vue
+++ Udemy/Vue-TheCompleteGuide/19_CompositionAPI/src/App.vue
@@ -1,22 +1,43 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+  </section>
+  <section class="container">
+    <h2>{{ secondUser.name }}</h2>
+    <h3>{{ secondUser.age }}</h3>
   </section>
 </template>
 
 <script>
 import { ref } from 'vue';
+import { reactive } from 'vue';
 
 export default {
   setup() {
-    const uName = ref('Vašek'); // ref create reactive value
+    // const uName = ref('Vašek'); // ref create reactive value
+    // const uAge = ref(25);
+
+    const userRefObject = ref({
+      name: 'Vašek',
+      age: 25,
+    });
+
+    const userReactiveObject = reactive({
+      name: 'Vašek',
+      age: 25,
+    });
 
     setTimeout(function () {
-      uName.value = 'Váša';
+      userRefObject.value.name = 'Váša';
+      userRefObject.value.age = 30;
+      userReactiveObject.name = 'Váša';
+      userReactiveObject.age = 30;
     }, 2000);
 
     return {
-      userName: uName,
+      user: userRefObject,
+      secondUser: userReactiveObject,
     };
   },
   // data() {
