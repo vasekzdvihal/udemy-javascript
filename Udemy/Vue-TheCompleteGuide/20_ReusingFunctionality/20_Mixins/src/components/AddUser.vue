6e639fb309aa5e4d58885ff33db2a707a230bf3c
Vášek Zdvihal <zdvihal@gmail.com>
2021-10-31 22:54:24 +0100

    Vue.js - Mixins, Mixins folder moved

---
+++ Udemy/Vue-TheCompleteGuide/20_ReusingFunctionality/20_Mixins/src/components/AddUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+    <p>Do you want to continue with adding a user?</p>
+  </user-alert>
+  <section>
+    <h2>Add a User</h2>
+    <button @click="showAlert">Add User</button>
+  </section>
+</template>
+
+<script>
+import alertMixin from '../mixins/alert'
+import UserAlert from './UserAlert';
+
+export default {
+  mixins: [alertMixin],
+  components: { // can't share components
+    UserAlert,
+  },
+};
+</script>
---
+++ Udemy/Vue-TheCompleteGuide/20_ReusingFunctionality/20_Mixins/src/components/DeleteUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script>
+import UserAlert from './UserAlert.vue';
+import alertMixin from '../mixins/alert';
+
+export default {
+  mixins: [alertMixin],
+  data() {
+    return {
+      alertTitle: 'Delete user?',
+      alertIsVisible: true, // components options override mixin
+    }
+  },
+  components: {
+    UserAlert,
+  },
+};
+</script>
