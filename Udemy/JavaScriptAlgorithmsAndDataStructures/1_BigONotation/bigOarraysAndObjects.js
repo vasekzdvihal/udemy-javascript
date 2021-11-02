643b1353db93ffe0b15e6695f21e38a5cfd11530
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-02 22:38:11 +0100

    BigO objects and arrays

---
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/1_BigONotation/bigOarraysAndObjects.js
@@ -0,0 +1,28 @@
+// https://cs.slides.com/colt_steele/built-in-data-structures-25
+
+// Objects
+// Insertion - O(1)
+// Removal - O(1)
+// Searching - O(N)
+// Access - O(1)
+
+// Object.keys -   O(N)
+// Object.values -   O(N)
+// Object.entries -   O(N)
+// hasOwnProperty -   O(1)
+
+// Arrays
+// Insertion -   It depends.... in beggining O(N), if as last O(1), we need to reindex
+// Removal -   It depends....
+// Searching -   O(N)
+// Access -   O(1)
+
+// push -   O(1)
+// pop -   O(1)
+// shift -   O(N)
+// unshift -   O(N)
+// concat -   O(N)
+// slice -   O(N)
+// splice -   O(N)
+// sort -   O(N * log N)
+// forEach/map/filter/reduce/etc. -   O(N)
