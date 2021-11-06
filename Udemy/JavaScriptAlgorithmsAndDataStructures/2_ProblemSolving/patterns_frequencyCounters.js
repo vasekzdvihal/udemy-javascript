864d036eb5ff5064094291bf82af1d238a093fa2
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-06 10:25:47 +0100

    Problem Solving Patterns - Frequency Counter Exercise with teacher solution

--- Udemy/JavaScriptAlgorithmsAndDataStructures/2_ProblemSolving/patterns_frequencyCounters.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/2_ProblemSolving/patterns_frequencyCounters.js
@@ -74,3 +74,32 @@ function validAnagram(str1, str2) {
   }
   return true;
 }
+
+function validAnagramTeacherSolution(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+  console.log(lookup)
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+console.log(validAnagramTeacherSolution('anagrams', 'nagaramm'));
