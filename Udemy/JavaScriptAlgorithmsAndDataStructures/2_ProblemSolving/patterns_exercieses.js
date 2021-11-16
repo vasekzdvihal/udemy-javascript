409085b61a1d8b59ab2535f364ad3594a028f123
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-16 22:48:43 +0100

    Problem solving patterns - exercises 8 and 9

--- Udemy/JavaScriptAlgorithmsAndDataStructures/2_ProblemSolving/patterns_exercieses.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/2_ProblemSolving/patterns_exercieses.js
@@ -1,3 +1,4 @@
+
 console.log('-----------------');
 console.log('Coding Exercise 3');
 console.log('-----------------');
@@ -97,3 +98,118 @@ function isSubsequence2(str1, str2) {
   if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
   return isSubsequence(str1, str2.slice(1))
 }
+
+console.log('-----------------');
+console.log('Coding Exercise 7');
+console.log('-----------------');
+console.log(maxSubarraySum([100,200,300,400], 2)); // 700
+console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
+console.log(maxSubarraySum([-3, 4, 0,-2, 6, -1], 2)); // 5
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, -4, -2, 1], 2)); // 5
+console.log(maxSubarraySum([2, 3], 3)); // null
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if(arr.length < num) return null;
+  for(let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+console.log('-----------------');
+console.log('Coding Exercise 8');
+console.log('-----------------');
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest subarray
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5,4] is smallest subarray
+console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is greater than t
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
+// teacher
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+
+  while(start < nums.length) {
+    // if current window doesn't add up to the given sum then move the window to right
+    if(total < sum && end < nums.length) {
+      total += nums[end];
+      end++;
+    }
+    // if current window adds up to at least the sum given then we can shrink the window
+    else if(total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    // current total less than required total but we reach the end
+    else {
+      break;
+    }
+  }
+  return minLen === Infinity ? 0 : minLen;
+}
+
+console.log('-----------------');
+console.log('Coding Exercise 9');
+console.log('-----------------');
+console.log(findLongestSubstring('')); // 0
+console.log(findLongestSubstring('rithmschool')); // 7
+console.log(findLongestSubstring('thisisawesome')); // 6
+console.log(findLongestSubstring('thecatinthehat')); // 7
+console.log(findLongestSubstring('bbbbbb')); // 1
+console.log(findLongestSubstring('thisislongestsubstring')); // 8
+console.log(findLongestSubstring('thisishowwedoit')); // 6
+// teacher
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+  }
+  return longest;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
