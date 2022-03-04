86fd00bd6e258c2f4b73492e524406d7011ed87d
Vášek Zdvihal <zdvihal@gmail.com>
2022-03-04 21:47:53 +0100

    exercises for recursion

--- Udemy/JavaScriptAlgorithmsAndDataStructures/3_Recursion/exercise.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/3_Recursion/exercise.js
@@ -1,5 +1,4 @@
 // Section 8:Recursion Problem Set
-
 // Coding Exercise 10: power
 function power(base, exponent) {
   if(exponent === 0) return 1;
@@ -24,5 +23,33 @@ console.log(factorial(4)) // 24
 console.log(factorial(7)) // 5040
 
 // Coding Exercise 12: productOfArray
+function productOfArray(arr) {
+  if(arr.length === 0) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
+}
+
+console.log('productOfArray');
+console.log(productOfArray([1, 2, 3])); // 6
+console.log(productOfArray([1, 2, 3, 10])); // 60
+
 // Coding Exercise 13: recursiveRange
+function recursiveRange(num) {
+  if(num === 0) return 0;
+  return num + recursiveRange(num - 1);
+}
+
+console.log('recursiveRange');
+console.log(recursiveRange(6)); // 21
+console.log(recursiveRange(10)); // 55
+
 // Coding Exercise 14: fib
+function fib(num) {
+  if(num <= 2) return 1;
+  return fib(num-1) + fib(num-2);
+}
+
+console.log('fib');
+console.log(fib(4)); // 3
+console.log(fib(10)); // 55
+console.log(fib(28)); // 317811
+console.log(fib(35)); // 9227465
