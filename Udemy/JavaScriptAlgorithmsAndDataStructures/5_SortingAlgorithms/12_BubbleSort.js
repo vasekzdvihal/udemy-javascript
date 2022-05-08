959bfa06628ad3e51319ada366aa30cf2e2700da
Vášek Zdvihal <zdvihal@gmail.com>
2022-05-08 19:11:01 +0200

    bubble sort (teacher implementation, noSwaps optimalization)

--- Udemy/JavaScriptAlgorithmsAndDataStructures/5_SortingAlgorithms/12_BubbleSort.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/5_SortingAlgorithms/12_BubbleSort.js
@@ -44,17 +44,19 @@ console.log(bubbleSort([1, 3, 23, 4, 5, 6, 7, 8, 9, 10])); // [1, 3, 4, 5, 6, 7,
 
 // teacher version
 function bubbleSort2(arr) {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
-      console.log(arr, arr[j], arr[j + 1]);
+      noSwaps = true;
       if (arr[j] > arr[j + 1]) {
         // SWAP!
         const temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
-    console.log('ONE PASS');
+    if(noSwaps) break;
   }
   return arr;
 }
