12445f01fcde64fd1ebe920d9251dbe0fb222f97
Vášek Zdvihal <zdvihal@gmail.com>
2022-12-26 13:54:38 +0100

    Data structures, Doubly linked list - get

--- Udemy/JavaScriptAlgorithmsAndDataStructures/6_DataStructures/20_DoublyLinkedLists.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/6_DataStructures/20_DoublyLinkedLists.js
@@ -136,11 +136,44 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  // GET (get a node at a specific index)
+  // If the index is less than 0 or greater or equal to the length, return null
+  // If the index is less than or equal to half the length of the list
+  //  Loop through the list starting from the head and loop towards the middle
+  //  Return the node once it is found
+  // If the index is greater than half the length of the list
+  //  Loop through the list starting from the tail and loop towards the middle
+  //  Return the node once it is found
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count;
+    let current;
+
+
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+
+    return current;
+  }
 }
 
 const list = new DoublyLinkedList();
 
-console.log('>>> push')
+console.log('>>> push');
 list.push('Hello');
 list.push('Bonjour');
 list.push('Howdy');
@@ -148,14 +181,18 @@ list.push('Hola');
 list.push('Ciao');
 list.print();
 
-console.log('>>> pop')
+console.log('>>> pop');
 list.pop();
 list.print();
 
-console.log('>>> shift')
+console.log('>>> shift');
 list.shift();
 list.print();
 
-console.log('>>> unshift')
+console.log('>>> unshift');
 list.unshift('Hello');
+list.print();
+
+console.log('>>> get');
+console.log(list.get(2));
 list.print();
