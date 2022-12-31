22f0b8fe6cca5fcc7fe2bda19699ac5a9e5dac24
Vášek Zdvihal <zdvihal@gmail.com>
2022-12-31 20:10:27 +0100

    Data structures, Trees - BTS insert

--- Udemy/JavaScriptAlgorithmsAndDataStructures/6_DataStructures/22_BinarySearchTree.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/6_DataStructures/22_BinarySearchTree.js
@@ -49,6 +49,61 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  // INSERTING A NODE
+  // This function should accept a value
+  // Create a new node with that value
+  // Starting at the root
+  //  Check if there is a root, if not - the root now becomes that new node!
+  //  If there is a root, check if the value of the new node is greater than or less than the value of the root
+  //  If it is greater
+  //    Check to see if there is a node to the right
+  //      If there is, move to that node and repeat these steps
+  //      If there is not, add that node as the right property
+  //  If it is less
+  //    Check to see if there is a node to the left
+  //      If there is, move to that node and repeat these steps
+  //      If there is not, add that node as the left property
+  // Return the Binary Search Tree
+  insert(val) {
+    const newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if(val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
+
+console.log('>>> insert');
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+console.log(tree);
+
