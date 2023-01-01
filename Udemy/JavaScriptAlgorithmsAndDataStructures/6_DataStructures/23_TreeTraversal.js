2c90ccaa1c018bb3b872f8f79a0d42747b55c8ec
Vášek Zdvihal <zdvihal@gmail.com>
2023-01-01 16:56:37 +0100

    Data structures, Tree traversal - deep first - in order

--- Udemy/JavaScriptAlgorithmsAndDataStructures/6_DataStructures/23_TreeTraversal.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/6_DataStructures/23_TreeTraversal.js
@@ -141,6 +141,29 @@ class BinarySearchTree {
     traverse(current);
     return data;
   }
+
+  // Depth-first search - In-order
+  // - Create a variable to store the values of nodes visited
+  // - Store the root of the BST in a variable called current
+  // - Write a helper function which accepts a node
+  //   - If the node has a left property, call the helper function with the left property on the node
+  //   - Push the value of the node to the variable that stores the values
+  //   - If the node has a right property, call the helper function with the right property on the node
+  // - Invoke the helper function with the current variable
+  // - Return the array of values
+  DFSInOrder() {
+    const data = [];
+    const current = this.root;
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.val);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(current);
+    return data;
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -154,4 +177,5 @@ tree.insert(20);
 console.log(tree.BFS()); // [10, 6, 15, 3, 8, 20]
 console.log(tree.DFSPreOrder()); // [10, 6, 3, 8, 15, 20]
 console.log(tree.DFSPostOrder()); // [3, 8, 6, 20, 15, 10]
+console.log(tree.DFSInOrder()); // [3, 6, 8, 10, 15, 20]
 console.log();
