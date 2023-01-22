a2fea052fea149f4c4e4800e25f7fa67268ceb74
Vášek Zdvihal <zdvihal@gmail.com>
2023-01-22 12:46:05 +0100

    Data structures, Hash table - values, keys

--- Udemy/JavaScriptAlgorithmsAndDataStructures/6_DataStructures/25_HashTables.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/6_DataStructures/25_HashTables.js
@@ -102,6 +102,38 @@ class HashTable {
     }
     return undefined;
   }
+
+  // KEYS
+  // Loops through the hash table array and returns an array of keys in the table
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+
+  // VALUES
+  // Loops through the hash table array and returns an array of values in the table
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
 }
 
 let ht = new HashTable(17);
@@ -119,3 +151,6 @@ console.log(ht.get("maroon"));
 console.log(ht.get("yellow"));
 console.log(ht.get("olive"));
 console.log(ht.get("nonexistent"));
+
+console.log('keys', ht.keys());
+console.log(ht.values());
