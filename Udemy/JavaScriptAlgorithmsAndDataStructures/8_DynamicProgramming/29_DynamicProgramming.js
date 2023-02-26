3ca5f28ba8106b0fb977f6d6e3c1c8da4e125251
Vášek Zdvihal <zdvihal@gmail.com>
2023-02-26 13:34:32 +0100

    Data structures, Dynamic programming - Intro

---
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/8_DynamicProgramming/29_DynamicProgramming.js
@@ -0,0 +1,15 @@
+// -------------------
+// Dynamic Programming
+// -------------------
+
+// Definition
+// A method for solving a complex problem by breaking it down into a collection of simpler
+// subproblems, solving each of those subproblems just once, and storing their solutions
+
+// Overlapping Subproblems
+// A problem is said to have overlapping subproblems if it can be broken down into
+// subproblems which are reused several times
+
+// Optimal Substructure
+// A problem is said to have optimal substructure if an optimal solution can be
+// constructed from optimal solutions of its subproblems
