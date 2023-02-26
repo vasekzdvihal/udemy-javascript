73066f993324658928725d7b17877f2524ae05f9
Vášek Zdvihal <zdvihal@gmail.com>
2023-02-26 11:22:01 +0100

    Data structures, Dijkstras algorithm - intro, weighted graph

---
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/7_DijktrasAlgorithm/28_DijkstrasAlgorithm.js
@@ -0,0 +1,19 @@
+// --------------------
+// Dijkstra's Algorithm
+// --------------------
+
+// What is it?
+// A weighted graph algorithm that finds the shortest path between two nodes
+
+// Who was he?
+// Edsger Wybe Dijkstra was a Dutch computer scientist and mathematical scientist
+// He was a pioneer in the field of computer science and is considered the father of the discipline of algorithmics
+// He was awarded the 1972 Turing Award for his work on shortest path finding algorithms
+
+// Why is it useful?
+// Used in GPS systems
+// Used in networking routing protocols
+// Used in many other fields
+// Used in Google Maps
+
+
