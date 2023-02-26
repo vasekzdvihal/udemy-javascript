dc798b87191756e39b7d60732c3a7ef2da5f8ae8
Vášek Zdvihal <zdvihal@gmail.com>
2023-02-26 14:24:36 +0100

    Data structures, Dynamic programming - Fibonnaci, memoizeed

--- Udemy/JavaScriptAlgorithmsAndDataStructures/8_DynamicProgramming/29_FibonacciSequence.js
+++ Udemy/JavaScriptAlgorithmsAndDataStructures/8_DynamicProgramming/29_FibonacciSequence.js
@@ -11,5 +11,15 @@ function fib(n) {
 console.log('Plain old recursive solution');
 console.log(fib(4));
 
+// Memoized solution
+function fibMemo(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  const res = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
 
+console.log('Memoized solution');
+console.log(fibMemo(4));
 
