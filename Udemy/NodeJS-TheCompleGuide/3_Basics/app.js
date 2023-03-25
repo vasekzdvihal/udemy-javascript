df307b8a71bd730a20200fe6d895da27dfc6af3c
Vášek Zdvihal <zdvihal@gmail.com>
2023-03-25 20:28:37 +0100

    Node.js - Understanding the Basics, Redirecting Request

--- Udemy/NodeJS-TheCompleGuide/3_Basics/app.js
+++ Udemy/NodeJS-TheCompleGuide/3_Basics/app.js
@@ -10,11 +10,30 @@
 // / - absolute path
 // just the name - core module
 const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.write('<html lang="en">');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
+  res.write('<html lang="en">');
   res.write('<head><title>My first page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
