2e9c64bbfc67b0fb4d002534e2c927d22c15ab84
Vášek Zdvihal <zdvihal@gmail.com>
2023-03-26 12:05:17 +0200

    Node.js - Understanding the Basics, Using the Node Modules System

--- Udemy/NodeJS-TheCompleGuide/3_Basics/app.js
+++ Udemy/NodeJS-TheCompleGuide/3_Basics/app.js
@@ -10,48 +10,9 @@
 // / - absolute path
 // just the name - core module
 const http = require('http');
-const fs = require('fs');
 
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    res.write('<html lang="en">');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body>');
-    res.write(' <form action="/message" method="POST">')
-    res.write('  <input type="text" name="message"><button type="submit">Send</button>')
-    res.write(' </form>')
-    res.write('</body>');
-    res.write('</html>');
-    return res.end();
-  }
-
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split('=')[1];
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html lang="en">');
-  res.write('<head><title>My first page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
