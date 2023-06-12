e3318224a3b8bb0c40af3ef0c12fa369a1c187dd
Vášek Zdvihal <zdvihal@gmail.com>
2023-06-12 23:26:36 +0200

    Node.js - Working with Express.js, Adding a 404 Error Page

--- Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/app.js
+++ Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>'); // Send error 404 page not found
+});
+
 app.listen(3000); // Starts a server on port 3000 => http://localhost:3000/
