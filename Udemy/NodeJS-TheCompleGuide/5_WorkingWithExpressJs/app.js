88bf4fb5801ef70ccfa09f1835b4f38e31a801fb
Vášek Zdvihal <zdvihal@gmail.com>
2023-06-12 23:23:50 +0200

    Node.js - Working with Express.js, Using Express Router

--- Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/app.js
+++ Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/app.js
@@ -3,25 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/', (req, res,next) => {
-  next(); // Allows the request to continue to the next middleware in line
-  // We call next() here because we want to continue to the next middleware (and this will be call for every request)
-});
-
-app.use('/add-product', (req, res,next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-  // We don't call next() here because we don't want to continue to the next middleware
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>'); // Sends a response
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000); // Starts a server on port 3000 => http://localhost:3000/
