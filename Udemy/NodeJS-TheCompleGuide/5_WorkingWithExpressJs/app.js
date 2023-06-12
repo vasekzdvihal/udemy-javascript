f7a15049b79d03c9ffb75ca4d96e15abec8a33b5
Vášek Zdvihal <zdvihal@gmail.com>
2023-06-12 23:13:32 +0200

    Node.js - Working with Express.js, Parsing Incoming Requests

--- Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/app.js
+++ Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/app.js
@@ -1,22 +1,26 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use('/', (req, res,next) => {
-  console.log('This always runs!');
   next(); // Allows the request to continue to the next middleware in line
   // We call next() here because we want to continue to the next middleware (and this will be call for every request)
 });
 
 app.use('/add-product', (req, res,next) => {
-  console.log('Add product middleware!');
-  res.send('<h1>The add product page</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
   // We don't call next() here because we don't want to continue to the next middleware
 });
 
-app.use('/', (req, res,next) => {
-  console.log('In another middleware!');
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!</h1>'); // Sends a response
 });
 
