15d19e5714ffbe7070af173293b2b1d3f038fb5b
Vášek Zdvihal <zdvihal@gmail.com>
2023-06-17 11:09:10 +0200

    Node.js - Working with Express.js, Serving HTML Pages

--- Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/routes/admin.js
+++ Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res,next) => {
-  res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')); // Sends a response
   // We don't call next() here because we don't want to continue to the next middleware
 });
 
