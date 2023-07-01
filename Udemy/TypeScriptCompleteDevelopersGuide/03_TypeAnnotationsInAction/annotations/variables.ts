be3d8e03a91a1bdbe0f399c07fcd49a59c9ec234
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-01 10:46:11 +0200

    TypeScript - Type Annotations in Action, Object Literal Annotations

--- Udemy/TypeScriptCompleteDevelopersGuide/03_TypeAnnotationsInAction/annotations/variables.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/03_TypeAnnotationsInAction/annotations/variables.ts
@@ -7,3 +7,17 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {
+
+}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number, y: number } = { x: 10, y: 20 }
