13ebfd7aaf984e02c4ba986aea870f17e6189abb
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-01 10:52:03 +0200

    TypeScript - Type Annotations in Action, Annotations Around Functions

--- Udemy/TypeScriptCompleteDevelopersGuide/03_TypeAnnotationsInAction/annotations/variables.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/03_TypeAnnotationsInAction/annotations/variables.ts
@@ -1,3 +1,4 @@
+// Variables
 let apples: number = 5;
 let speed: string = 'fast';
 let hasName: boolean = true;
@@ -5,7 +6,7 @@ let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
-// built in objects
+// Built in objects
 let now: Date = new Date();
 
 // Array
@@ -14,10 +15,13 @@ let myNumbers: number[] = [1, 2, 3, 4];
 let truths: boolean[] = [true, true, false];
 
 // Classes
-class Car {
-
-}
+class Car { }
 let car: Car = new Car();
 
 // Object literal
 let point: { x: number, y: number } = { x: 10, y: 20 }
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+}
