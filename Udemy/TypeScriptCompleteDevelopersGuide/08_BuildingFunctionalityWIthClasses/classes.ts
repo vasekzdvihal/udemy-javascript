fa75b83438c5d1f1f3b7a3c6945d9f90d4a683dc
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-02 13:52:40 +0200

    TypeScript - Building Functionality with Classes, Instance Method Modifiers

--- Udemy/TypeScriptCompleteDevelopersGuide/08_BuildingFunctionalityWIthClasses/classes.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/08_BuildingFunctionalityWIthClasses/classes.ts
@@ -1,9 +1,9 @@
 class Vehicle {
-    drive() : void {
+    public drive() : void {
         console.log('chugga chugga');
     }
 
-    honk() : void {
+    protected honk() : void {
         console.log('beep');
     }
 }
@@ -12,11 +12,15 @@ class Car extends Vehicle {
     drive() : void {
         console.log('vroom');
     }
+
+    startDrivingProcess() : void {
+        this.drive();
+        this.honk();
+    }
 }
 
 const vehicle = new Vehicle();
 const car = new Car();
 vehicle.drive();
-vehicle.honk();
-car.drive();
-car.honk();
+car.startDrivingProcess();
+// car.honk(); // error: Property 'honk' is protected and only accessible within class 'Vehicle' and its subclasses.ts(2445)
