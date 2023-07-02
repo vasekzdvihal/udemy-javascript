0270ce8ae40b338e4ef01d321fc78ba013c3a637
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-02 19:34:01 +0200

    Node.js - Working with Express.js, Styling our Pages

--- Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/routes/admin.js
+++ Udemy/NodeJS-TheCompleGuide/5_WorkingWithExpressJs/routes/admin.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
+const rootDir = require('../util/path');
 
 router.get('/add-product', (req, res,next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')); // Sends a response
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html')); // Sends a response
   // We don't call next() here because we don't want to continue to the next middleware
 });
 
