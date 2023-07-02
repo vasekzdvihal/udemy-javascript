ba58783d400a385d217e5169e3e6b4c160ff2af2
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-02 11:28:32 +0200

    TypeScript - Mastering Typed Arrays, Arrays in TS

---
+++ Udemy/TypeScriptCompleteDevelopersGuide/05_MasteringTypedArrays/arrays.ts
@@ -0,0 +1,11 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+// two-dimensional array
+const carsByMake = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+];
+
+
