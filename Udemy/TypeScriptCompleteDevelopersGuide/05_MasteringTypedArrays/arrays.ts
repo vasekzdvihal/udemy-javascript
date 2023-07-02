5e9edb34ca89ae3b44f16d9eef3aad7f955f6e6b
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-02 11:32:48 +0200

    TypeScript - Mastering Typed Arrays, Why Typed Arrays?

--- Udemy/TypeScriptCompleteDevelopersGuide/05_MasteringTypedArrays/arrays.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/05_MasteringTypedArrays/arrays.ts
@@ -8,4 +8,14 @@ const carsByMake = [
     ['camaro']
 ];
 
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
 
+// Prevent incompatible values
+// carMakers.push(100); // error
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+    return car.toUpperCase();
+});
