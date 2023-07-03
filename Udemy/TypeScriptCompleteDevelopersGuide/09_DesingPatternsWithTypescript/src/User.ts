8744b378ec772d26a1497872179b0c8fbf673345
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-03 21:29:27 +0200

    TypeScript - Design Patterns with TS, Using Type Definition Files

--- Udemy/TypeScriptCompleteDevelopersGuide/09_DesingPatternsWithTypescript/src/User.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/09_DesingPatternsWithTypescript/src/User.ts
@@ -8,6 +8,10 @@ class User {
     }
 
     constructor() {
-        this.name = faker.name.firstName();
+        this.name = faker.person.firstName();
+        this.location = {
+            lat: faker.location.latitude(),
+            lng: faker.location.longitude()
+        };
     }
 }
