0ea25e08aaa51f33b01273094c3e63635f2ba265
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-03 21:22:14 +0200

    TypeScript - Design Patterns with TS,Generating Random Data

---
+++ Udemy/TypeScriptCompleteDevelopersGuide/09_DesingPatternsWithTypescript/src/User.ts
@@ -0,0 +1,13 @@
+import { faker } from '@faker-js/faker';
+
+class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    }
+
+    constructor() {
+        this.name = faker.name.firstName();
+    }
+}
