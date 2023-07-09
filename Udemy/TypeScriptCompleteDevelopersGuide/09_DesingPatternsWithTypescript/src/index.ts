2f09798801a7879508d35f94a42b0562c5de6287
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-09 17:15:38 +0200

    TypeScript - Design Patterns with TS, Adding Markers

--- Udemy/TypeScriptCompleteDevelopersGuide/09_DesingPatternsWithTypescript/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/09_DesingPatternsWithTypescript/src/index.ts
@@ -1,9 +1,12 @@
 /// <reference types="@types/google.maps" />
 
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
-new CustomMap('map');
+const customMap = new CustomMap('map');
+const user = new User();
+const company = new Company();
 
-
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
