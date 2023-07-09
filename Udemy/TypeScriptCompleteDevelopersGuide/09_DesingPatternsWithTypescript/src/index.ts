464d9377cd1fd00e763218fae7804bbee5cf4ad5
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-09 15:43:24 +0200

    TypeScript - Design Patterns with TS, Exploring Type Definition Files

--- Udemy/TypeScriptCompleteDevelopersGuide/09_DesingPatternsWithTypescript/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/09_DesingPatternsWithTypescript/src/index.ts
@@ -1,10 +1,14 @@
 /// <reference types="@types/google.maps" />
 
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-const company = new Company();
+import LatLngAltitude = google.maps.LatLngAltitude;
 
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
