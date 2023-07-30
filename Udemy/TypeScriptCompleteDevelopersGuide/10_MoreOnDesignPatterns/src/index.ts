01ed7c6808a7a5a8dfdab8dafa6188d004956ac6
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-30 22:06:29 +0200

    TypeScript - More on Design Patterns, Separating Swapping and Comparsion

--- Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/index.ts
@@ -1,32 +1,9 @@
-class Sorter {
-    constructor(public collection: number[] | string) { }
-
-    sort(): void {
-        const { length} = this.collection;
-
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-
-                // All of this only works if collection is number[]
-                // If collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
+import {NumbersCollection} from "./NumbersCollection";
+import {Sorter} from "./sorter";
 
 
-                // Only going to work if collection is a string
-                if (typeof this.collection === 'string') {
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
