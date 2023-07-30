be903bee40dc902eedb04563a3a73072bea62ee3
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-30 18:49:16 +0200

    TypeScript - More on Design Patterns, Sorting Implementation

--- Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/index.ts
@@ -6,7 +6,17 @@ class Sorter {
     }
 
     sort(): void {
+        const { length} = this.collection;
 
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
     }
 }
 
