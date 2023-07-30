3b47b860c0aa3786002924b3f2d6baa31ed947bb
Vášek Zdvihal <zdvihal@gmail.com>
2023-07-30 19:12:35 +0200

    TypeScript - More on Design Patterns, Type Guards

--- Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/index.ts
@@ -1,19 +1,26 @@
 class Sorter {
-    collection: number[];
-
-    constructor(collection: number[]) {
-        this.collection = collection;
-    }
+    constructor(public collection: number[] | string) { }
 
     sort(): void {
         const { length} = this.collection;
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+
+                // All of this only works if collection is number[]
+                // If collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+
+
+                // Only going to work if collection is a string
+                if (typeof this.collection === 'string') {
+
                 }
             }
         }
