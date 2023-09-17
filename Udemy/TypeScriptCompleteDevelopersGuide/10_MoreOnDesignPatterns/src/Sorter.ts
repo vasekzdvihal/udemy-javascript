c2d2f9b9e8d9b57b4f87c96ca7e4bd15b6a41dde
Vášek Zdvihal <zdvihal@gmail.com>
2023-09-17 15:17:23 +0200

    TypeScript - More on Design Patterns, Abstract classes

--- Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/Sorter.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/Sorter.ts
@@ -4,7 +4,11 @@ interface Sortable {
     swap(leftIndex: number, rightIndex: number): void;
 }
 
-export class Sorter {
+export abstract class Sorter {
+    abstract length: number;
+    abstract compare(leftIndex: number, rightIndex: number): boolean;
+    abstract swap(leftIndex: number, rightIndex: number): void;
+
     sort(): void {
         for (let i = 0; i < this.length; i++) {
             for (let j = 0; j < this.length - i - 1; j++) {
