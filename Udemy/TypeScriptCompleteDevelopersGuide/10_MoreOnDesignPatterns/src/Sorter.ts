ec4aa76c6259878ca6ea7210307324ecca6f7506
Vášek Zdvihal <zdvihal@gmail.com>
2023-09-17 14:27:35 +0200

    TypeScript - More on Design Patterns, Issues with Ihneritance

--- Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/Sorter.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/Sorter.ts
@@ -5,15 +5,11 @@ interface Sortable {
 }
 
 export class Sorter {
-    constructor(public collection: Sortable ) { }
-
     sort(): void {
-        const { length} = this.collection;
-
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection.compare(j, j+1)) {
-                    this.collection.swap(j, j+1);
+        for (let i = 0; i < this.length; i++) {
+            for (let j = 0; j < this.length - i - 1; j++) {
+                if (this.compare(j, j+1)) {
+                    this.swap(j, j+1);
                 }
             }
         }
