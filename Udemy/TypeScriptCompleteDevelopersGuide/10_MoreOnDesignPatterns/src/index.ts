505f7db5518b39dc95dde40455cad03b9c970aec
Vášek Zdvihal <zdvihal@gmail.com>
2023-09-17 14:16:53 +0200

    TypeScript - More on Design Patterns, Linked list

--- Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/10_MoreOnDesignPatterns/src/index.ts
@@ -1,6 +1,7 @@
 import {NumbersCollection} from "./NumbersCollection";
 import {Sorter} from "./Sorter";
 import {CharactersCollection} from "./CharactersCollection";
+import {MyLinkedList} from "./LinkedList";
 
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
@@ -13,3 +14,14 @@ const charactersCollection = new CharactersCollection('Xaayb');
 const charSorter = new Sorter(charactersCollection);
 charSorter.sort();
 console.log(charactersCollection.data);
+
+
+const linkedList = new MyLinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
