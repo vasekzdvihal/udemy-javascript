92628b96537c9fd5b5c630fd3faee4a6c1f2d2af
Vášek Zdvihal <zdvihal@gmail.com>
2023-11-09 00:01:21 +0100

    TypeScript - Reusable Code, Converting Row Values

--- Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/src/CsvFileReader.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/src/CsvFileReader.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { dateStringToDate } from "./utils";
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -6,8 +7,17 @@ export class CsvFileReader {
   constructor(public filename: string) { }
 
   read() : void {
-    this.data = fs.readFileSync(this.filename, {
-      encoding: 'utf-8'
-    }).split('\n').map((r) => r.split(','));
+    this.data = fs.readFileSync(this.filename, { encoding: 'utf-8' })
+      .split('\n')
+      .map((row: string): string[] => row.split(','))
+      .map((row: string[]): any => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+        ]
+      });
   }
 }
