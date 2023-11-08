87b5223065d1cd86f636b74bbc60fa6da14077d4
Vášek Zdvihal <zdvihal@gmail.com>
2023-11-08 23:53:05 +0100

    TypeScript - Reusable Code, Extracting CSV Reading

--- Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
+++ Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
@@ -1,12 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n').map((r) => r.split(','));
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
 let manUnitedWins = 0;
 var MatchResult;
 (function (MatchResult) {
@@ -14,7 +10,7 @@ var MatchResult;
     MatchResult["AwayWin"] = "A";
     MatchResult["Draw"] = "D";
 })(MatchResult || (MatchResult = {}));
-for (let match of matches) {
+for (let match of reader.data) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
