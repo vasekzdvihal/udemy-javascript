08367789057d6a4101adc24dd7a7d369149fad69
Vášek Zdvihal <zdvihal@gmail.com>
2023-11-08 23:39:30 +0100

    TypeScript - Reusable Code, Running an Analysis

--- Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
+++ Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
@@ -7,4 +7,13 @@ const fs_1 = __importDefault(require("fs"));
 const matches = fs_1.default.readFileSync('football.csv', {
     encoding: 'utf-8'
 }).split('\n').map((r) => r.split(','));
-console.log(matches);
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
