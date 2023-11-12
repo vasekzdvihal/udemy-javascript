1b2f6eb0a39b507deb05465a5ffeb35d67cc6e23
Vášek Zdvihal <zdvihal@gmail.com>
2023-11-12 13:04:55 +0100

    TypeScript - Reusable Code, Refactoring Interface based approach

--- Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
+++ Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CsvFileReader_1 = require("./CsvFileReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new CsvFileReader_1.CsvFileReader('football.csv');
-reader.read();
+const MatchReader_1 = require("./MatchReader");
+const CsvFileReader_1 = require("./CsvFileReader");
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
--- Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/inheritance/CsvFileReader.js
+++ Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/inheritance/CsvFileReader.js
@@ -13,7 +13,8 @@ class CsvFileReader {
     read() {
         this.data = fs_1.default.readFileSync(this.filename, { encoding: 'utf-8' })
             .split('\n')
-            .map((row) => row.split(','));
+            .map((row) => row.split(','))
+            .map(this.mapRow);
     }
 }
 exports.CsvFileReader = CsvFileReader;
