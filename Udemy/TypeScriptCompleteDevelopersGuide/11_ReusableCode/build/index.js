5cf92fcfe4acc973798f6d1fdad07dc48af58a56
Vášek Zdvihal <zdvihal@gmail.com>
2023-11-12 20:55:35 +0100

    TypeScript - Reusable Code, Implmementing an Analyzer Class

--- Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
+++ Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const MatchResult_1 = require("./MatchResult");
 const MatchReader_1 = require("./MatchReader");
 const CsvFileReader_1 = require("./CsvFileReader");
 // Create an object that satisfies the 'DataReader' interface
@@ -8,13 +7,3 @@ const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 // Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(`Man United won ${manUnitedWins} games`);
