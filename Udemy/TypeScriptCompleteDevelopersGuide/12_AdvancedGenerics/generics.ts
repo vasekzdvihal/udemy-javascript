1471e575195cd1afc9497a31ea81006264338a19
Vášek Zdvihal <zdvihal@gmail.com>
2023-11-26 14:52:24 +0100

    TypeScript - Advanced Generics, Generic Contraints

--- Udemy/TypeScriptCompleteDevelopersGuide/12_AdvancedGenerics/generics.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/12_AdvancedGenerics/generics.ts
@@ -43,3 +43,29 @@ function printAnything<T>(arr: T[]): void {
     console.log(arr[i]);
   }
 }
+
+// Generic constrains
+class Car {
+  print() {
+    console.log('I am i car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()])
+printHousesOrCars<Car>([new Car(), new Car()])
