53d9a31fc97f891662d86af88faebaa5f85a0efe
Vášek Zdvihal <zdvihal@gmail.com>
2023-12-03 15:28:07 +0100

    TypeScript - Web Framework, Dynamic Array Creation

--- Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/index.ts
@@ -9,3 +9,9 @@ user.set({ name: 'newName', age: 999 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
+
+user.on('change', () => {
+  console.log('Hi');
+});
+
+console.log(user);
--- Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/models/User.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/models/User.ts
@@ -3,7 +3,7 @@ interface UserProps {
   age?: number,
 }
 
-type Callback = () => {};
+type Callback = () => void;
 
 export class User {
   events: { [key: string]: Callback[] } = {};
@@ -19,7 +19,9 @@ export class User {
   }
 
   on(eventName: string, callback: Callback) {
-
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   }
 }
 
