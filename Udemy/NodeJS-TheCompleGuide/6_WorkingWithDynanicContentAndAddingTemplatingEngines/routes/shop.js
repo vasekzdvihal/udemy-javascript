4e3a375b7a2deaa44e27214dbf1694e5812bfecc
Vášek Zdvihal <zdvihal@gmail.com>
2023-12-03 23:30:13 +0100

    Node.js - Working with Dynamic Content and Adding Templating Engines, Sharing Data Across Requests and Users

---
+++ Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/routes/shop.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+  console.log(adminData.products);
+  res.sendFile(path.join(rootDir, 'views', 'shop.html')); // Sends a response
+});
+
+module.exports = router;
