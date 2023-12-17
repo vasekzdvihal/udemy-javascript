7cb3b688458b7bbf2352129cb7d8a5a9e30542c9
Vášek Zdvihal <zdvihal@gmail.com>
2023-12-17 14:31:36 +0100

    Node.js - Working with Dynamic Content and Adding Templating Engines, Finishing the Pug Template

--- Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/routes/admin.js
+++ Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/routes/admin.js
@@ -8,12 +8,12 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add Product'})
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
 })
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
+  products.push({ title: req.body.title, path: '/admin/add-product' });
   res.redirect('/');
 });
 
--- Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/routes/shop.js
+++ Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/routes/shop.js
@@ -8,7 +8,7 @@ const adminData = require('./admin');
 router.get('/', (req, res, next) => {
   console.log(adminData.products);
   const products = adminData.products;
-  res.render('shop', { prods: products, docTitle: "Shop" }); // Renders a template with the given template engine
+  res.render('shop', { prods: products, pageTitle: "Shop", path: '/shop' }); // Renders a template with the given template engine
 });
 
 module.exports = router;
