ec298c6da7808cf340df656e38d3ac0aac3ee0dc
Vášek Zdvihal <zdvihal@gmail.com>
2023-12-17 20:22:33 +0100

    Node.js - Working with Dynamic Content and Adding Templating Engines, Converting our Project to Handlebars

--- Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/routes/shop.js
+++ Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/routes/shop.js
@@ -8,7 +8,7 @@ const adminData = require('./admin');
 router.get('/', (req, res, next) => {
   console.log(adminData.products);
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: "Shop", path: '/shop' }); // Renders a template with the given template engine
+  res.render('shop', { prods: products, pageTitle: "Shop", path: '/shop', hasProducts: products.length > 0 }); // Renders a template with the given template engine
 });
 
 module.exports = router;
