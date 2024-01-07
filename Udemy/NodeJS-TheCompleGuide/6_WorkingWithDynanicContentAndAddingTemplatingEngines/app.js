90f6f8bf99a68fe510c4e116c9d4815cfaf33688
Vášek Zdvihal <zdvihal@gmail.com>
2024-01-07 12:57:47 +0100

    Node.js - Working with Dynamic Content and Adding Templating Engines, Working with EJS

--- Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/app.js
+++ Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/app.js
@@ -9,12 +9,14 @@ const app = express();
 // app.set("view engine", "pug");
 
 // Handlebars
-app.engine('hbs', expressHbs({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', expressHbs({
+//   layoutsDir: 'views/layouts/',
+//   defaultLayout: 'main-layout',
+//   extname: 'hbs'
+// }));
+
+// EJS
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
