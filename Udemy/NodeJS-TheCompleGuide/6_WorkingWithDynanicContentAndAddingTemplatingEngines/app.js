380f7f49fe510f1e1af4ecc9c86010d14a42db4e
Vášek Zdvihal <zdvihal@gmail.com>
2024-01-07 13:43:34 +0100

    Node.js - Working with Dynamic Content and Adding Templating Engines, Wrap up

--- Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/app.js
+++ Udemy/NodeJS-TheCompleGuide/6_WorkingWithDynanicContentAndAddingTemplatingEngines/app.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// PUG (pug is default template engine)
-// app.set("view engine", "pug");
-
-// Handlebars
-// app.engine('hbs', expressHbs({
-//   layoutsDir: 'views/layouts/',
-//   defaultLayout: 'main-layout',
-//   extname: 'hbs'
-// }));
-
-// EJS
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', 'views'); // views is default, but this is how you would change it
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
