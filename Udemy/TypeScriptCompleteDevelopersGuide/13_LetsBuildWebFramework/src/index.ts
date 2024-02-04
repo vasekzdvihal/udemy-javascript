bd37c53b94ca6b7c833650498b5a5aef8da7bf09
Vášek Zdvihal <zdvihal@gmail.com>
2024-02-04 11:56:56 +0100

    TypeScript - Web Framework, Adding JSON Server

--- Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/index.ts
@@ -2,25 +2,15 @@ import { User } from './models/User';
 
 const user = new User({name: "myName", age: 12 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
-
-user.set({ name: 'newName', age: 999 });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
-
 user.on('change', () => {
-  console.log('Hi');
+  console.log('Change #1');
 });
 user.on('change', () => {
-  console.log('Hi 2');
+  console.log('Change #2');
 });
 user.on('save', () => {
-  console.log('Hi 43');
+  console.log('Save was triggered');
 });
-
-console.log(user);
 
 user.trigger("change");
 user.trigger("save");
