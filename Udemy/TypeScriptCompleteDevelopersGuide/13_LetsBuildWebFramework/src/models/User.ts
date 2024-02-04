9937753db16769598628df947b65ae39be584c4e
Vášek Zdvihal <zdvihal@gmail.com>
2024-02-04 14:23:30 +0100

    TypeScript - Web Framework, Refactoring with Composition

--- Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/models/User.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/models/User.ts
@@ -6,10 +6,10 @@ interface UserProps {
   age?: number,
 }
 
-type Callback = () => void;
+
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
+
 
   constructor(private data: UserProps) { }
 
@@ -21,23 +21,9 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
 
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
 
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
 
-    handlers.forEach(callback => {
-      callback();
-    })
-  }
 
   fetch(): void {
     axios.get(`http://localhost:3000/users/${this.get('id')}`)
