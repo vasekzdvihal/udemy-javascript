9550926773da6bae9f5cfe2c53f6af7c6ee9dba6
Vášek Zdvihal <zdvihal@gmail.com>
2024-02-04 14:13:08 +0100

    TypeScript - Web Framework, Successfully Fetching Model Data

--- Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/index.ts
@@ -1,27 +1,14 @@
-// import { User } from './models/User';
-//
-// const user = new User({name: "myName", age: 12 });
-//
-// user.on('change', () => {
-//   console.log('Change #1');
-// });
-// user.on('change', () => {
-//   console.log('Change #2');
-// });
-// user.on('save', () => {
-//   console.log('Save was triggered');
-// });
-//
-// user.trigger("change");
-// user.trigger("save");
+import { User } from "./models/User";
 
-import axios from 'axios';
 
 console.log('Hello from 13_LetsBuildWebFramework');
 
-// axios.post('http://localhost:3000/users', {
-//   name: 'myName',
-//   age: 20,
-// });
-//
-// axios.get('http://localhost:3000/users/1');
+const user = new User({ id: 1 });
+
+user.fetch();
+
+console.log(user.get('name'));
+
+setTimeout(() => {
+  console.log(user.get('name'));
+}, 4000);
