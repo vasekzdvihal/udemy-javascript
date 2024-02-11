54eda05e24bcfd9b1e4a90f12e6e043ed3c8c52a
Vášek Zdvihal <zdvihal@gmail.com>
2024-02-11 17:40:36 +0100

    TypeScript - Web Framework, Reminder on Accessors

--- Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/index.ts
@@ -5,8 +5,14 @@ console.log('Hello from 13_LetsBuildWebFramework');
 
 const user = new User({ name: 'brand new record', age: 2344234 });
 
-user.events.on('change', () => {
-  console.log('Changed #1');
-});
-
-user.events.trigger("change");
+// A quick reminder on accessors
+// class Person {
+//   constructor(public firstName: string, public lastName: string) {}
+//
+//   get fullName(): string { // get instead of function
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
+//
+// const person = new Person('Vasa', 'Sasa');
+// console.log(person.fullName);
