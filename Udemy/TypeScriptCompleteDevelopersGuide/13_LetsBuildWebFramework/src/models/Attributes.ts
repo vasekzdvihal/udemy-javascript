5d42343aa6900efc227833ac91bda67646ea704e
Vášek Zdvihal <zdvihal@gmail.com>
2024-02-11 13:50:58 +0100

    TypeScript - Web Framework, Extracting an Attributes Class

---
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/models/Attributes.ts
@@ -0,0 +1,14 @@
+import {UserProps} from "./User";
+
+export class Attributes<T extends object> {
+  constructor(private data: T) {
+  }
+
+  get(propName: string): (number | string) {
+    return this.data[propName];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
