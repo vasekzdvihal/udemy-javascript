ab55a665ff3ab0341127af623b9a4de6a1ca41d5
Vášek Zdvihal <zdvihal@gmail.com>
2024-04-27 21:37:40 +0200

    TypeScript - Web Framework, Adding Methods to the User

--- Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/views/UserForm.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/views/UserForm.ts
@@ -12,8 +12,8 @@ export class UserForm {
     }
   }
 
-  onSetRandomAgeClick() {
-    console.log('Set random age');
+  onSetRandomAgeClick = (): void => {
+    this.model.setRandomAge();
   }
 
   template(): string {
