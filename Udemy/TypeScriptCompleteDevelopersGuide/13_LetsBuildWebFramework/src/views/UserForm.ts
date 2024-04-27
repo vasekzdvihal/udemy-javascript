3498070c352663fd3281d75db60a735c1f5fd68b
Vášek Zdvihal <zdvihal@gmail.com>
2024-04-27 18:14:55 +0200

    TypeScript - Web Framework, Building the UserForm

---
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/views/UserForm.ts
@@ -0,0 +1,11 @@
+export class UserForm {
+  parent: Element;
+  template(): string {
+    return `
+      <div>
+        <h1>User Form</h1>
+        <input type="text">
+      </div>
+    `;
+  }
+}
