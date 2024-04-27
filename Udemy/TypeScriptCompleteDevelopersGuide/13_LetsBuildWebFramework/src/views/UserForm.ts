38dafe8a86c48833f3accc4a68b5cd8a812ecf6b
Vášek Zdvihal <zdvihal@gmail.com>
2024-04-27 18:56:00 +0200

    TypeScript - Web Framework, Binding Event Handlers

--- Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/views/UserForm.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/13_LetsBuildWebFramework/src/views/UserForm.ts
@@ -4,12 +4,17 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick
+      'click:button': this.onButtonClick,
+      'mouseover:h1': this.onHeaderHover
     }
   }
 
   onButtonClick(): void {
     console.log ('Hi there!')
+  }
+
+  onHeaderHover(): void {
+    console.log ('H1 Was hovered over!');
   }
 
   template(): string {
@@ -22,10 +27,23 @@ export class UserForm {
     `;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
