4fc042bca09069ae6847a749cca9471ae40a3e11
Vášek Zdvihal <zdvihal@gmail.com>
2024-09-08 13:09:39 +0200

    TypeScript - Decorators, More on Decorators

--- Udemy/TypeScriptCompleteDevelopersGuide/15_Decorators/decorators.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/15_Decorators/decorators.ts
@@ -1,23 +1,35 @@
+@classDecorator
 class Boat {
   @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boat color is ${this.color}`;
   }
 
   @logError("Oops, boat was sunk")
-  pilot(): void {
-    // throw new Error();
-    console.log ('swish');
+  pilot(@parameterDecorator speed: string, @parameterDecorator generateWake: boolean): void {
+    if (speed === 'fast') {
+      console.log ('swish');
+    } else {
+      console.log ('nothing');
+    }
   }
 }
 
-function testDecorator(target: any, key: string) {
-  console.log (target); // target.color won't work due limitations with prototype
-  console.log (key);
+function classDecorator(constructor: typeof Boat) {
+  console.log (constructor);
 }
 
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log (key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  // console.log(target); // target.color won't work due limitations with prototype
+  console.log(key);
+}
 
 function logError(errorMessage: string) {
   return function(target: any, key: string, desc: PropertyDescriptor): void {
