dc746acc0067b1d6909a4a90a4394155fb2c48fb
Vášek Zdvihal <zdvihal@gmail.com>
2025-01-26 12:20:46 +0100

    TypeScript - Advanced Express and TS Integration, The 'Get' Decorator

--- Udemy/TypeScriptCompleteDevelopersGuide/16_AdvancedExpressAndTSIntegration/server/build/controllers/LoginController.js
+++ Udemy/TypeScriptCompleteDevelopersGuide/16_AdvancedExpressAndTSIntegration/server/build/controllers/LoginController.js
@@ -9,6 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var routes_1 = require("./decorators/routes");
 var LoginController = /** @class */ (function () {
     function LoginController() {
     }
@@ -16,7 +17,7 @@ var LoginController = /** @class */ (function () {
         res.send("\n      <form method=\"POST\">\n        <div>\n          <label for=\"emails\">Email</label>\n          <input name=\"email\">\n        </div>\n        <div>\n          <label for=\"password\">Password</label>\n          <input name=\"password\" type=\"password\">\n        </div>\n        <button>Submit</button>\n      </form>\n    ");
     };
     __decorate([
-        get('/login'),
+        (0, routes_1.get)('/login'),
         __metadata("design:type", Function),
         __metadata("design:paramtypes", [Object, Object]),
         __metadata("design:returntype", void 0)
