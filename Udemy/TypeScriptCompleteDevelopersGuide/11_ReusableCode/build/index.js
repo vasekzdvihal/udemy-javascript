b92bd03862b05bf63b047ad34a14e67f48ccf9d0
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-01 09:10:34 +0100

    Remove build folders and node_modules from git tracking

--- Udemy/TypeScriptCompleteDevelopersGuide/11_ReusableCode/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const MatchReader_1 = require("./MatchReader");
-const Summary_1 = require("./Summary");
-// Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
-const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
-matchReader.load();
-const summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
-summary.buildAndPrintReport(matchReader.matches);
