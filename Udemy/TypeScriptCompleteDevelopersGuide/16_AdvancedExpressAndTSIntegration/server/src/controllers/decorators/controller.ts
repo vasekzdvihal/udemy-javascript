eee0142ba1727e75663146f99eb56e18ebb212dd
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-01 10:25:30 +0100

    TypeScript - Advanced Express and TS Integration, Body Validators & Testing Automated Validation

--- Udemy/TypeScriptCompleteDevelopersGuide/16_AdvancedExpressAndTSIntegration/server/src/controllers/decorators/controller.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/16_AdvancedExpressAndTSIntegration/server/src/controllers/decorators/controller.ts
@@ -2,6 +2,25 @@ import 'reflect-metadata';
 import { AppRouter } from "../../AppRouter";
 import { Methods } from "./Methods";
 import { MetadataKeys } from "./MetadataKeys";
+import { NextFunction, RequestHandler, Response, Request } from "express";
+
+function bodyValidators(keys: string[]): RequestHandler {
+  return function(req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid request');
+      return;
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send('Invalid request');
+        return;
+      }
+    }
+
+    next();
+  }
+}
 
 export function controller(routePrefix: string) {
   return function(target: Function) {
@@ -14,10 +33,13 @@ export function controller(routePrefix: string) {
 
       const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
 
+      const requiredBodyProps = Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || [];
+      const validator = bodyValidators(requiredBodyProps);
+
       if (path) {
         // Inside router is definition of values that's are possible and TS check if MethodsEnum are valid.
         // So when we add value (like testTest = "testTest"), this will be considered error because there is no testTest in router definition
-        router[method](`${routePrefix}${path}`,  ...middlewares, routeHandler);
+        router[method](`${routePrefix}${path}`,  ...middlewares, validator, routeHandler);
       }
     });
   }
