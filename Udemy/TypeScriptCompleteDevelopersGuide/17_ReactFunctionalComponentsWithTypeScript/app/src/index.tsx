3195301b9e98af7793b6ebe1e24ac852ba560c65
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-01 14:54:18 +0100

    TypeScript - React and Redux Class Components with Typescript, Hadling Component State

--- Udemy/TypeScriptCompleteDevelopersGuide/17_ReactFunctionalComponentsWithTypeScript/app/src/index.tsx
+++ Udemy/TypeScriptCompleteDevelopersGuide/17_ReactFunctionalComponentsWithTypeScript/app/src/index.tsx
@@ -10,8 +10,24 @@ interface AppProps {
 }
 
 class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+
   render() {
-    return <div>Hi there. Color is {this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        <span>{this.state.counter}</span>
+      </div>
+    );
   }
 }
 
