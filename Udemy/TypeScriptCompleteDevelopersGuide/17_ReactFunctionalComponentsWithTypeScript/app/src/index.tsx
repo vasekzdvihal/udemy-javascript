d297f2d0c5feea09e4f3e0430925da232e706067
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-01 15:08:26 +0100

    TypeScript - React and Redux Class Components with Typescript, Confusing Component State

--- Udemy/TypeScriptCompleteDevelopersGuide/17_ReactFunctionalComponentsWithTypeScript/app/src/index.tsx
+++ Udemy/TypeScriptCompleteDevelopersGuide/17_ReactFunctionalComponentsWithTypeScript/app/src/index.tsx
@@ -9,8 +9,20 @@ interface AppProps {
   color?: string;
 }
 
+// interface AppState {
+//   counter: number;
+// }
+
 class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+  // You can create constructor like this, but you need create interface like AppState above this, to pass as second
+  // generic type in component to avoid error when React pass ReadOnly generic instead.
+  // constructor(props: AppProps) {
+  //   super(props);
+  //
+  //   this.state = { counter: 0 }
+  // }
+
+  state = { counter: 0 }
 
   onIncrement = (): void => {
     this.setState({ counter: this.state.counter + 1 });
