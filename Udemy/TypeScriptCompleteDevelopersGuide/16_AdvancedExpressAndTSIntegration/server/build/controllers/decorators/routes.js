6e7a9bb45fbf891c8c2ca7285ee9fecf3c1de742
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-01 08:45:41 +0100

    TypeScript - Advanced Express and TS Integration, Defining a RouteBinder

--- Udemy/TypeScriptCompleteDevelopersGuide/16_AdvancedExpressAndTSIntegration/server/build/controllers/decorators/routes.js
+++ Udemy/TypeScriptCompleteDevelopersGuide/16_AdvancedExpressAndTSIntegration/server/build/controllers/decorators/routes.js
@@ -1,10 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.get = void 0;
+exports.path = exports.del = exports.put = exports.post = exports.get = void 0;
 require("reflect-metadata");
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
+function routeBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', method, target, key);
+        };
     };
 }
-exports.get = get;
+exports.get = routeBinder('get');
+exports.post = routeBinder('post');
+exports.put = routeBinder('put');
+exports.del = routeBinder('del');
+exports.path = routeBinder('patch');
