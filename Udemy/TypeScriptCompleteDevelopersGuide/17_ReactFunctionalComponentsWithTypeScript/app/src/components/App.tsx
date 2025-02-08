570b921f53a7a486db312e8d43be713e29a4e343
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-08 17:35:10 +0100

    TypeScript - React and Redux Class Components with Typescript, Connecting a Component to Redux

--- Udemy/TypeScriptCompleteDevelopersGuide/17_ReactFunctionalComponentsWithTypeScript/app/src/components/App.tsx
+++ Udemy/TypeScriptCompleteDevelopersGuide/17_ReactFunctionalComponentsWithTypeScript/app/src/components/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { fetchTodos, Todo } from "../actions";
+import { StoreState } from "../reducers";
+import { connect } from "react-redux";
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+
+}
+
+class _App extends React.Component<AppProps> {
   render() {
     return  <div>Hi there!!!</div>
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return {
+    todos: todos
+  }
+}
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App)
