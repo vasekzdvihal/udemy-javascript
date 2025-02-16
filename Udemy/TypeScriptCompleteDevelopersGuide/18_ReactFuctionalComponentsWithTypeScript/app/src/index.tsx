ae85b9a345e2e11afba96a68ceae8b77e7d4d048
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-16 10:07:39 +0100

    TypeScript - React Functional Components with TypeScript, State with TypeScript

--- Udemy/TypeScriptCompleteDevelopersGuide/18_ReactFuctionalComponentsWithTypeScript/app/src/index.tsx
+++ Udemy/TypeScriptCompleteDevelopersGuide/18_ReactFuctionalComponentsWithTypeScript/app/src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import GuestList from "./state/GuestList";
 
 const el = document.getElementById("root");
 
@@ -7,7 +8,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <GuestList />
     </div>
   );
 };
