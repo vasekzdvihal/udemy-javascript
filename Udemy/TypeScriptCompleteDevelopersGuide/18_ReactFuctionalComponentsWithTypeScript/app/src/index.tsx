653b10e09123e39e925da223829748efeb48b398
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-16 09:35:35 +0100

    TypeScript - React Functional Components with TypeScript, File Extensions and Starting Up React

---
+++ Udemy/TypeScriptCompleteDevelopersGuide/18_ReactFuctionalComponentsWithTypeScript/app/src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+  );
+};
+
+root.render(<App />);
