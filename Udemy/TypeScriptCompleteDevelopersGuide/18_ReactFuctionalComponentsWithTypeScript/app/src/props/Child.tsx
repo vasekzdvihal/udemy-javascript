443d4d11593b7125c85d791b31ae5f614ef0d9ea
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-16 09:53:45 +0100

    TypeScript - React Functional Components with TypeScript, Annotations with Children

--- Udemy/TypeScriptCompleteDevelopersGuide/18_ReactFuctionalComponentsWithTypeScript/app/src/props/Child.tsx
+++ Udemy/TypeScriptCompleteDevelopersGuide/18_ReactFuctionalComponentsWithTypeScript/app/src/props/Child.tsx
@@ -2,12 +2,22 @@ import React from "react";
 
 interface ChildProps {
   color: string;
+  onClick: () => void;
+  children?: React.ReactNode;
 }
 
-export const Child = ({ color }: ChildProps) => {
-  return <div>Hi there! Color is {color}</div>
+export const Child = ({ color, onClick, children }: ChildProps) => {
+  return <div>
+    <h1>Hi there! Color is {color}</h1>
+    <button onClick={onClick}>Click me!</button>
+    <span>{children}</span>
+  </div>
 }
 
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-  return <div>Hi there! Color is {color}</div>
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick, children }) => {
+  return <div>
+    <h1>Hi there! Color is {color}</h1>
+    <button onClick={onClick}>Click me!</button>
+    <span>{children}</span>
+  </div>
 }
