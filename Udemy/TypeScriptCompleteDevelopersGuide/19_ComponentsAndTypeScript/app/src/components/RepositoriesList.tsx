c5978ae3233a16682960533c61f1beb91debe44d
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-22 14:48:18 +0100

    TypeScript - Components and TypeScript, Calling an Action Creator & Binding Action Creators

--- Udemy/TypeScriptCompleteDevelopersGuide/19_ComponentsAndTypeScript/app/src/components/RepositoriesList.tsx
+++ Udemy/TypeScriptCompleteDevelopersGuide/19_ComponentsAndTypeScript/app/src/components/RepositoriesList.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useState } from 'react';
+import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    searchRepositories(term);
   }
 
   return <div>
