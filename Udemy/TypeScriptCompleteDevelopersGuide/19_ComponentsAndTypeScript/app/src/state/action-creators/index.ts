12d18ecb88998aae6848e800f01b04e506271a90
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-22 13:41:27 +0100

    TypeScript - Components and TypeScript, Adding Request Logic

--- Udemy/TypeScriptCompleteDevelopersGuide/19_ComponentsAndTypeScript/app/src/state/action-creators/index.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/19_ComponentsAndTypeScript/app/src/state/action-creators/index.ts
@@ -1,14 +1,29 @@
 import axios from 'axios';
 import { ActionType } from "../action-types";
 
-const searchRepositories = (term: string) => {
+const API_URL = 'https://registry.npmjs.org/-/v1/search'
+
+export const searchRepositories = (term: string) => {
   return async (dispatch: any) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     });
 
     try {
+      const { data } = await axios.get(API_URL, {
+        params: {
+          text: term
+        }
+      });
 
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      })
     } catch (err) {
       if (err instanceof Error) {
         dispatch({
