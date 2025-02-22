0f11d8351786bcbf280292b88723686a64532bc4
Vášek Zdvihal <zdvihal@gmail.com>
2025-02-22 10:00:20 +0100

    TypeScript - Components and TypeScript, Adding an Action Type Enum

--- Udemy/TypeScriptCompleteDevelopersGuide/19_ComponentsAndTypeScript/app/src/state/reducers/repositoriesReducer.ts
+++ Udemy/TypeScriptCompleteDevelopersGuide/19_ComponentsAndTypeScript/app/src/state/reducers/repositoriesReducer.ts
@@ -5,32 +5,37 @@ interface RepositoriesState {
 }
 
 interface SearchRepositoriesAction {
-  type: 'search_repositories';
+  type: ActionType.SEARCH_REPOSITORIES;
 }
 interface SearchRepositoriesSuccessAction {
-  type: 'search_repositories_success';
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
   payload: string[];
 }
 interface SearchRepositoriesErrorAction {
-  type: 'search_repositories_error';
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
   payload: string;
 }
 
-const reducer = (
-  state: RepositoriesState,
-  action: SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction
-): RepositoriesState => {
+type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction // | is called type union
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
 
   // if (action.type === 'search_repositories_success') is type guard and inside this if we know for 100% that
   // we're using SearchRepositoriesSuccessAction interface. Same think happened with switch-case, behave like type guard
   // so we know (and TS knows) that inside payload is either string[] or string;
 
   switch (action.type) {
-    case 'search_repositories':
+    case ActionType.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: [] }
-    case 'search_repositories_success':
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
       return { loading: false, error: null, data: action.payload }
-    case 'search_repositories_error':
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
       return { loading: false, error: action.payload, data: [] }
     default:
       return state;
