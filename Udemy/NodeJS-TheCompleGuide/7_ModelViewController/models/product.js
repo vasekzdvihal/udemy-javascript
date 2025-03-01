5b55acc6117b72d6c206979073213da35548674d
Vášek Zdvihal <zdvihal@gmail.com>
2025-03-01 14:39:01 +0100

    Node.js - The Model View Controller, Storing Data in Files Via the Model

--- Udemy/NodeJS-TheCompleGuide/7_ModelViewController/models/product.js
+++ Udemy/NodeJS-TheCompleGuide/7_ModelViewController/models/product.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const products = [];
 
 module.exports = class Product {
@@ -5,11 +8,30 @@ module.exports = class Product {
     this.title = title;
   }
 
+
   save() {
-    products.push(this);
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent.toString());
+      }
+
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products, null, 2), (error) => {
+        console.log(error);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return []; // We can throw error here, we want just empty array.
+      }
+
+      return JSON.parse(fileContent.toString());
+    });
   }
 }
