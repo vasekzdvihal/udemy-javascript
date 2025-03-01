c6e074fe41b0f5e4e369e52285d6705688940126
Vášek Zdvihal <zdvihal@gmail.com>
2025-03-01 09:37:06 +0100

    Node.js - The Model View Controller, Module Introduction

---
+++ Udemy/NodeJS-TheCompleGuide/7_ModelViewController/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views'); // views is default, but this is how you would change it
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminRoutes.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {pageTitle: 'Page Not Found', path: ""});
+})
+
+app.listen(3000); // Starts a server on port 3000 => http://localhost:3000/
---
+++ Udemy/NodeJS-TheCompleGuide/7_ModelViewController/routes/shop.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: "Shop",
+    path: '/shop',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  }); // Renders a template with the given template engine
+});
+
+module.exports = router;
