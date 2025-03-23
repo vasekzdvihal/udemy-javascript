3995b36e89ab8d974f5a77ac561aae42a2b684b6
Vášek Zdvihal <zdvihal@gmail.com>
2025-03-23 13:56:52 +0100

    Node.js - Dynamic Routes & Advanced Models, Displaying Cart Items on the Cart Page

--- Udemy/NodeJS-TheCompleGuide/9_DynamicRoutes&AdvancedModels/controllers/shop.js
+++ Udemy/NodeJS-TheCompleGuide/9_DynamicRoutes&AdvancedModels/controllers/shop.js
@@ -33,9 +33,22 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+
+      res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path: '/cart',
+        products: cartProducts
+      });
+    })
   });
 };
 
@@ -44,6 +57,7 @@ exports.postCart = (req, res, next) => {
   Product.findById(prodId, (product) => {
     Cart.addProduct(prodId, product.price);
   });
+  res.redirect('/cart');
 }
 
 exports.getOrders = (req, res, next) => {
