52dc89895a56a408368083cba179962502c8526a
Vášek Zdvihal <zdvihal@gmail.com>
2025-03-23 13:23:36 +0100

    Node.js - Dynamic Routes & Advanced Models, Adding the Product-Delete Functionality

--- Udemy/NodeJS-TheCompleGuide/9_DynamicRoutes&AdvancedModels/models/product.js
+++ Udemy/NodeJS-TheCompleGuide/9_DynamicRoutes&AdvancedModels/models/product.js
@@ -50,6 +50,17 @@ module.exports = class Product {
     });
   }
 
+  static delete(id) {
+    getProductsFromFile(products => {
+      const updatedProducts = products.filter(p => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          
+        }
+      })
+    });
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
--- Udemy/NodeJS-TheCompleGuide/9_DynamicRoutes&AdvancedModels/routes/admin.js
+++ Udemy/NodeJS-TheCompleGuide/9_DynamicRoutes&AdvancedModels/routes/admin.js
@@ -19,4 +19,7 @@ router.post('/add-product', adminController.postAddProduct);
 router.get('/edit-product/:productId', adminController.getEditProduct);
 router.post('/edit-product', adminController.postEditProduct);
 
+// 
+router.post('/delete-product')
+
 module.exports = router;
