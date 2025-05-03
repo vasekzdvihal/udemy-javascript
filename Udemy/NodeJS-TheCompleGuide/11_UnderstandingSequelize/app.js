fe2d63df1635d2dc1bc4b7843c994b54934dc7f9
Vášek Zdvihal <zdvihal@gmail.com>
2025-05-03 10:33:31 +0200

    Node.js - Understanding Sequelize, Adding a One-To-Many Relationship

--- Udemy/NodeJS-TheCompleGuide/11_UnderstandingSequelize/app.js
+++ Udemy/NodeJS-TheCompleGuide/11_UnderstandingSequelize/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,10 +24,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
-.then(result => {
-  console.log(`Database lodaded, will listen on port 3000!`);
-  app.listen(3000);  
-}).catch(error => {
-  console.log(err);
-});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+  .sync({ force: true })
+  .then(result => {
+    console.log(`Database lodaded, will listen on port 3000!`);
+    app.listen(3000);  
+  })
+  .catch(error => {
+    console.log(err);
+  });
