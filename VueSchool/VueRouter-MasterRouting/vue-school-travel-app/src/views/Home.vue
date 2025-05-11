03c249da21d081575215661c6d832c5767d09cce
Vášek Zdvihal <zdvihal@gmail.com>
2025-05-11 13:33:03 +0200

    Vue Router 4 Master Routing in Vue.js - Fundamentals, Introduction and Basic Routes

---
+++ VueSchool/VueRouter-MasterRouting/vue-school-travel-app/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div class="destinations">
+      <router-link
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="destination.slug"
+      >
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" :alt="destination.name">
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import sourceData from '../../data.json';
+import { ref } from 'vue';
+
+const destinations = ref(sourceData.destinations);
+</script>
