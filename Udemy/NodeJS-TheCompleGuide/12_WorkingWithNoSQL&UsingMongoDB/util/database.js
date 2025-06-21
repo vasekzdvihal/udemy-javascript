7e174136c8e75766aa4ff015d9c17c6979f5fe19
Vášek Zdvihal <zdvihal@gmail.com>
2025-06-21 21:40:34 +0200

    Node.js - Working with NoSQL & Using MongoDB,, Creating the Database Connection

--- Udemy/NodeJS-TheCompleGuide/12_WorkingWithNoSQL&UsingMongoDB/util/database.js
+++ Udemy/NodeJS-TheCompleGuide/12_WorkingWithNoSQL&UsingMongoDB/util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = (callback) => {
+MongoClient.connect('XXX')
+  .then(client => {
+    console.log('Connected!');
+    _db = client.db();
+    callback();
+  })
+  .catch(err => {
+    console.error(err);
+    throw err;
+  });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
