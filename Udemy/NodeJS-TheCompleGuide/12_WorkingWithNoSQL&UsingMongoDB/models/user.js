3325f21583eaa7d46745fbb0ac89f4b386f96138
Vášek Zdvihal <zdvihal@gmail.com>
2025-07-05 10:34:40 +0200

    Node.js - Working with NoSQL & Using MongoDB, Working on Cart Items & Orders

--- Udemy/NodeJS-TheCompleGuide/12_WorkingWithNoSQL&UsingMongoDB/models/user.js
+++ Udemy/NodeJS-TheCompleGuide/12_WorkingWithNoSQL&UsingMongoDB/models/user.js
@@ -4,9 +4,11 @@ const mongodb = require('mongodb');
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this.id = _id;
   }
 
   save() {
@@ -14,6 +16,23 @@ class User {
     return db
     .collection('users')
     .insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProduct = this.cart.items.findIndex(cp => {
+      return cp._id === product._id;
+    });
+
+    
+    const updatedCart = { items: [{ ...product, quantity: 1 }]};
+    
+    const db = getDb();
+    db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) }, 
+        { $set: { cart: updatedCart }}
+      );
   }
 
   static findById(userId) {
