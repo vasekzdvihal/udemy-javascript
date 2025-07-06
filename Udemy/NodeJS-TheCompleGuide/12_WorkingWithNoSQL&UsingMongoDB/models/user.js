a7610354680c2410e25d97fcb9400608910569f4
Vášek Zdvihal <zdvihal@gmail.com>
2025-07-06 12:29:06 +0200

    Node.js - Working with NoSQL & Using MongoDB, Deleting Cart Items

--- Udemy/NodeJS-TheCompleGuide/12_WorkingWithNoSQL&UsingMongoDB/models/user.js
+++ Udemy/NodeJS-TheCompleGuide/12_WorkingWithNoSQL&UsingMongoDB/models/user.js
@@ -68,6 +68,20 @@ class User {
       });
   }
 
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(i => {
+      return i.productId.toString() !== productId.toString();
+    });
+    
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this.id) },
+        { $set: { cart: { items: updatedCartItems } }}
+      )
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
